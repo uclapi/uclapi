4446f466be9d243550b569d64cfa3f14f2c62177
Wilhelm Klopp <wil.klopp@gmail.com>
2017-01-13 19:07:20 +0000

    Add CRUD API

--- backend/uclapi/dashboard/models.py
+++ backend/uclapi/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .app_helpers import generate_api_token, generate_app_id
 
 
 # Create your models here.
@@ -14,11 +15,26 @@ class User(models.Model):
 
 
 class App(models.Model):
+    id = models.CharField(
+        max_length=20,
+        primary_key=True,
+        default=generate_app_id
+    )
     user = models.ForeignKey(User, related_name='user')
     name = models.CharField(max_length=1000)
-    api_token = models.CharField(max_length=1000)
+    api_token = models.CharField(
+        max_length=1000,
+        unique=True,
+        default=generate_api_token
+    )
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
+
+    def regenerate_token(self):
+        new_token = generate_api_token()
+        self.api_token = new_token
+        self.save()
+        return self.api_token
 
 
 class APICall(models.Model):
