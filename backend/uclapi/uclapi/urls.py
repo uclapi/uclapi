8bce4cebcb8a2011409bbe26ed15ebe40fc03de9
Wilhelm Klopp <wil.klopp@gmail.com>
2017-01-13 15:09:52 +0000

    Fix dashboard rename

--- backend/uclapi/uclapi/urls.py
+++ backend/uclapi/uclapi/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^dashboard/', include('devconsole.urls')),
+    url(r'^dashboard/', include('dashboard.urls')),
     url(r'^roombookings/', include('roombookings.urls')),
 ]
