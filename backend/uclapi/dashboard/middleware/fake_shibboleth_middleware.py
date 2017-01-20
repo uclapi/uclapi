ddbb2dd53f86bc2317959c71a4b576db624d1419
Wilhelm Klopp <wil.klopp@gmail.com>
2017-01-20 20:47:27 +0000

    Improve middleware checking

--- backend/uclapi/dashboard/middleware/fake_shibboleth_middleware.py
+++ backend/uclapi/dashboard/middleware/fake_shibboleth_middleware.py
@@ -3,9 +3,6 @@ from django.utils.deprecation import MiddlewareMixin
 
 class FakeShibbolethMiddleWare(MiddlewareMixin):
     def process_request(self, request):
-        if (
-            "convert-post-headers" in request.POST and
-            request.POST["convert-post-headers"] == "1"
-        ):
+        if request.POST.get("convert-post-headers") == "1":
             for key in request.POST:
                 request.META[key] = request.POST[key]
