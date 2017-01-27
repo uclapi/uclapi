283b7006952a8e061e5a66243e4cc2cac4384851
Christopher Hammond <christopherhammond13@gmail.com>
2017-01-27 16:09:16 +0000

    The request will not be post, so do not check for this

--- backend/uclapi/dashboard/views.py
+++ backend/uclapi/dashboard/views.py
@@ -10,58 +10,46 @@ def shibboleth_callback(request):
     # this view is user facing, so should return html error page
     # should auth user login or signup
     # then redirect to dashboard homepage
-
-    if request.method == 'POST':
-        try:
-            eppn = request.META['HTTP_EPPN']
-            groups = request.META['HTTP_UCLINTRANETGROUPS']
-            cn = request.META['HTTP_CN']
-            department = request.META['HTTP_DEPARTMENT']
-            given_name = request.META['HTTP_GIVENNAME']
-            display_name = request.META['HTTP_DISPLAYNAME']
-            employee_id = request.META['HTTP_EMPLOYEEID']
-        except KeyError as e:
-            context = {
-                "error": "Didn't receive all required Shibboleth data."
-            }
-            print(e)
-            return render(
-                request,
-                'shibboleth_error.html',
-                context=context,
-                status=400
-            )
-
-        try:
-            user = User.objects.get(email=eppn)
-        except ObjectDoesNotExist:
-            # create a new user
-            new_user = User(
-                email=eppn,
-                full_name=display_name,
-                given_name=given_name,
-                department=department,
-                cn=cn,
-                raw_intranet_groups=groups,
-                employee_id=employee_id
-            )
-
-            new_user.save()
-            request.session["user_id"] = new_user.id
-        else:
-            request.session["user_id"] = user.id
-
-        return redirect(index)
-    else:
+    try:
+        eppn = request.META['HTTP_EPPN']
+        groups = request.META['HTTP_UCLINTRANETGROUPS']
+        cn = request.META['HTTP_CN']
+        department = request.META['HTTP_DEPARTMENT']
+        given_name = request.META['HTTP_GIVENNAME']
+        display_name = request.META['HTTP_DISPLAYNAME']
+        employee_id = request.META['HTTP_EMPLOYEEID']
+    except KeyError as e:
         context = {
-            "error": "Request is not of type POST."
+            "error": "Didn't receive all required Shibboleth data."
         }
+        print(e)
         return render(
             request,
             'shibboleth_error.html',
             context=context,
             status=400
         )
+
+    try:
+        user = User.objects.get(email=eppn)
+    except ObjectDoesNotExist:
+        # create a new user
+        new_user = User(
+            email=eppn,
+            full_name=display_name,
+            given_name=given_name,
+            department=department,
+            cn=cn,
+            raw_intranet_groups=groups,
+            employee_id=employee_id
+        )
+
+        new_user.save()
+        request.session["user_id"] = new_user.id
+    else:
+        request.session["user_id"] = user.id
+
+    return redirect(index)
 
 
 def index(request):
