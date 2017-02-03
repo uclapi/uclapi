92d9070d88cb0c053303bbd1cbe717d76e5d4aea
Jaromir Latal <latal.jaromir@gmail.com>
2017-02-03 14:19:34 +0000

    Initial rate-limiting settings

--- backend/uclapi/uclapi/settings.py
+++ backend/uclapi/uclapi/settings.py
@@ -101,6 +101,17 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
