4bc5b4f21411d24cf79a03259a2d54400ae517b6
Wilhelm Klopp <wil.klopp@gmail.com>
2017-02-16 13:28:28 +0000

    Add Equipment test

--- backend/uclapi/roombookings/tests.py
+++ backend/uclapi/roombookings/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from itertools import chain
 
-from .helpers import _serialize_rooms
+from .helpers import _serialize_rooms, _serialize_equipment
 from .models import Room
 
 
@@ -79,3 +79,46 @@ class SerializationTestCase(SimpleTestCase):
                 }
             ]
         )
+
+
+class EquipmentSerializationTestCase(SimpleTestCase):
+
+        def test_serialize_equipment(self):
+            equipment_list = [
+                FakeModelClass(
+                    setid="LIVE-16-17",
+                    roomid="016",
+                    units=1,
+                    description="Chairs with Tables",
+                    siteid="267",
+                    type="FF"
+                ),
+                FakeModelClass(
+                    setid="LIVE-16-17",
+                    roomid="105",
+                    units=1,
+                    description="Software - Earth Sciences - self learning",
+                    siteid="002",
+                    type="FF"
+                )
+            ]
+            none_qs = Room.objects.none()
+            equipment_qs = list(chain(none_qs, equipment_list))
+            serialised_equipment = _serialize_equipment(equipment_qs)
+            self.assertEqual(
+                serialised_equipment, [
+                    {
+                        "type": "FF",
+                        "description": "Chairs with Tables",
+                        "units": 1
+                    },
+                    {
+                        "type": "FF",
+                        "description": (
+                            "Software - Earth Sciences"
+                            " - self learning"
+                        ),
+                        "units": 1
+                    }
+                ]
+            )
