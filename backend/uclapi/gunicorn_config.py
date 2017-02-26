48eaa999f3aebb0e5b194d2c8c8fa907efefde2f
Christopher Hammond <christopherhammond13@gmail.com>
2017-02-26 22:35:35 +0000

    Add gunicorn configuration

---
+++ backend/uclapi/gunicorn_config.py
@@ -0,0 +1,15 @@
+import multiprocessing
+
+bind = "127.0.0.1:9000"
+
+# Run cores * 4 + 1 workers in gunicorn
+# This is set deliberately high in case of long Oracle transactions locking Django up
+workers = multiprocessing.cpu_count() * 4 + 1
+threads = multiprocessing.cpu_count() * 4
+
+# Using gaiohttp because of the long blocking calls to the Oracle database
+worker_class = gaiohttp
+
+daemon = True
+
+proc_name = "uclapi_gunicorn"
