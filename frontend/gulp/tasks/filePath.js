b53f06673e4e7e0d7757866980735e8cb179c720
Henry Mortimer <henry@morti.plus.com>
2017-02-28 16:20:36 +0000

    added comment

--- frontend/gulp/tasks/filePath.js
+++ frontend/gulp/tasks/filePath.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var fileModules = require('./fileModules.json');
 
-var baseDir = path.resolve(__dirname, '../../../');
+var baseDir = path.resolve(__dirname, '../../../'); //gets absolute path to UCLAPI base directory
 var backendPath = 'backend/uclapi/';
 
 module.exports = function filePath(fileName){
