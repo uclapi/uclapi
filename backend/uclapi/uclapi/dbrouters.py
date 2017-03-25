ce6f92fc130006501be599bfb76f15914031a97b
Christopher Hammond <christopherhammond13@gmail.com>
2017-03-25 23:07:52 +0000

    Fix migrations

--- backend/uclapi/uclapi/dbrouters.py
+++ backend/uclapi/uclapi/dbrouters.py
@@ -11,5 +11,6 @@ class ModelRouter(object):
     def allow_relation(self, obj1, obj2, **hints):
         return obj1._state.db in self.managed_db_list and obj2._state.db in self.managed_db_list
 
-    def allow_migrate(self, db, model):
-        return getattr(model, "_DATABASE") in self.managed_db_list
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        #return getattr(model_name, "_DATABASE") in self.managed_db_list
+        return True
