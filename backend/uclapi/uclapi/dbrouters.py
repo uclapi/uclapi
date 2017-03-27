1aff0ad568979b6f3d81813fb7007587b7672f28
Christopher Hammond <christopherhammond13@gmail.com>
2017-03-27 15:50:00 +0100

    Rewritten router migration decisions

--- backend/uclapi/uclapi/dbrouters.py
+++ backend/uclapi/uclapi/dbrouters.py
@@ -3,14 +3,23 @@ class ModelRouter(object):
         self.managed_db_list = ['default', 'gencache']
 
     def db_for_read(self, model, **hints):
-        return getattr(model, "_DATABASE", "default")
+        return getattr(model, "_DATABASE")
 
     def db_for_write(self, model, **hints):
-        return getattr(model, "_DATABASE", "default")
+        return getattr(model, "_DATABASE")
 
     def allow_relation(self, obj1, obj2, **hints):
         return obj1._state.db in self.managed_db_list and obj2._state.db in self.managed_db_list
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        # Router should have no opinion on whether to allow a migration
+        if db == "default":
+            if (model_name == "BookingA") or (model_name == "BookingB"):
+                return False
+            else:
+                return True
+        elif db == "gencache":
+            if (model_name == "BookingA") or (model_name == "BookingB"):
+                return True
+            else:
+                return False
         return None
