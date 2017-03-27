f468f33f728a8072abb6758c3583a5b6208f6580
Christopher Hammond <christopherhammond13@gmail.com>
2017-03-27 19:26:12 +0100

    Make sure the _DATABASES setting is read from the model's Meta class

--- backend/uclapi/uclapi/dbrouters.py
+++ backend/uclapi/uclapi/dbrouters.py
@@ -3,10 +3,10 @@ class ModelRouter(object):
         self.managed_db_list = ['default', 'gencache']
 
     def db_for_read(self, model, **hints):
-        return getattr(model, "_DATABASE", "default")
+        return getattr(model._meta, "_DATABASE", "default")
 
     def db_for_write(self, model, **hints):
-        return getattr(model, "_DATABASE", "default")
+        return getattr(model._meta, "_DATABASE", "default")
 
     def allow_relation(self, obj1, obj2, **hints):
         return obj1._state.db in self.managed_db_list and obj2._state.db in self.managed_db_list
