523ed79e69e40b5276bb54eb8766ba3501014593
Wilhelm Klopp <wil.klopp@gmail.com>
2017-03-28 11:54:18 +0100

    Improve update_locations

--- backend/uclapi/roombookings/management/commands/update_locations.py
+++ backend/uclapi/roombookings/management/commands/update_locations.py
@@ -8,27 +8,27 @@ class Command(BaseCommand):
     help = "Fetches lat lng coordinates from Estates and puts them in our db"
 
     def handle(self, *args, **options):
-        room_data = json.loads(open('roombookings/management/commands/data/rooms.json').read())
+        with open(
+            'roombookings/management/commands/data/rooms.json', 'r'
+        ) as f:
+            room_data = json.loads(f.read())
 
         for room in room_data:
             siteid = room_data[room]["siteid"]
+            lat = room_data[room][
+                "location"]["coordinates"]["latitute"]
+            lng = room_data[room][
+                "location"]["coordinates"]["longitude"]
             try:
-                lat = room_data[room][
-                    "location"]["coordinates"]["latitute"]
-                lng = room_data[room][
-                    "location"]["coordinates"]["longitude"]
-                try:
-                    loc = Location.objects.get(siteid=siteid)
-                    loc.lat = lat
-                    loc.lng = lng
-                    loc.save()
-                except ObjectDoesNotExist:
-                    loc = Location(
-                        siteid=siteid,
-                        lat=lat,
-                        lng=lng
-                    )
-                    loc.save()
-            except:
-                pass
+                loc = Location.objects.get(siteid=siteid)
+                loc.lat = lat
+                loc.lng = lng
+                loc.save()
+            except ObjectDoesNotExist:
+                loc = Location(
+                    siteid=siteid,
+                    lat=lat,
+                    lng=lng
+                )
+                loc.save()
         self.stdout.write("All done")
