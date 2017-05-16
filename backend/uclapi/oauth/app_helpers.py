5bf1384fe098a410f82a01946a66642cd994ef5e
Christopher Hammond <christopherhammond13@gmail.com>
2017-05-16 21:20:58 +0100

    Add OAuth models

---
+++ backend/uclapi/oauth/app_helpers.py
@@ -0,0 +1,15 @@
+from binascii import hexlify
+import os
+
+def generate_user_token():
+    key = hexlify(os.urandom(30)).decode()
+    dashes_key = ""
+    for idx, char in enumerate(key):
+        if idx % 15 == 0 and idx != len(key)-1:
+            dashes_key += "-"
+        else:
+            dashes_key += char
+
+    final = "uclapi-user" + dashes_key
+
+    return final
