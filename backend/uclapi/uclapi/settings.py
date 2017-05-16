39a511aef844f5e14a232d62b65a719db89318dc
Christopher Hammond <christopherhammond13@gmail.com>
2017-05-16 21:21:48 +0100

    Add OAuth App to Django

--- backend/uclapi/uclapi/settings.py
+++ backend/uclapi/uclapi/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'dashboard',
     'roombookings',
+    'oauth',
     'opbeat.contrib.django',
     'raven.contrib.django.raven_compat',
     'corsheaders'
