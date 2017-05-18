cb3f6393de9f79f31e8514556aca86192c5a313a
Christopher Hammond <christopherhammond13@gmail.com>
2017-05-18 04:11:55 +0100

    [HOTFIX]: Force encoding to UTF-8 when loading the AUP

--- backend/uclapi/uclapi/settings.py
+++ backend/uclapi/uclapi/settings.py
@@ -208,5 +208,5 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/roombookings/.*$'
 
 # Fair use policy
-with open(os.path.join(BASE_DIR, 'uclapi/UCLAPIAcceptableUsePolicy.txt'), 'r') as fp:
+with open(os.path.join(BASE_DIR, 'uclapi/UCLAPIAcceptableUsePolicy.txt'), 'r', encoding='utf-8') as fp:
     FAIR_USE_POLICY = list(fp)
