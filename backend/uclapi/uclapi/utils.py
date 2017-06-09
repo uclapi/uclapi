2b9e31cfc54577620795785c0f8010e10052a57e
Jaromir Latal <latal.jaromir@gmail.com>
2017-06-09 01:13:11 +0100

    replacing strtobool with its built-in counterpart

--- backend/uclapi/uclapi/utils.py
+++
@@ -1,8 +0,0 @@
-def strtobool(x):
-    try:
-        b = x.lower() in ("true", "yes", "1", "y")
-        return b
-    except AttributeError:
-        return False
-    except NameError:
-        return False
