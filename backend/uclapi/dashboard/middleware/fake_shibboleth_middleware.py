b88abd98834529f1342d69e2e91b79efd68e5e8d
Christopher Hammond <christopherhammond13@gmail.com>
2017-06-10 17:12:40 +0100

    Add get parameter parsing for fakeshibboleth auto mode

--- backend/uclapi/dashboard/middleware/fake_shibboleth_middleware.py
+++ backend/uclapi/dashboard/middleware/fake_shibboleth_middleware.py
@@ -6,3 +6,10 @@ class FakeShibbolethMiddleWare(MiddlewareMixin):
         if request.POST.get("convert-post-headers") == "1":
             for key in request.POST:
                 request.META[key] = request.POST[key]
+
+        if request.GET.get("convert-get-headers") == "1":
+            for key in request.GET:
+                http_key = key.upper()
+                http_key.replace("-", "_")
+                http_key = "HTTP_" + http_key
+                request.META[http_key] = request.GET[key]
