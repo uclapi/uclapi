89d67d8cbcf40df69c8ab9b87289c3b111dab01b
Jaromir Latal <latal.jaromir@gmail.com>
2017-06-24 07:58:08 +0200

    tests for oauth_token_check decorator (-permission)

--- backend/uclapi/oauth/tests.py
+++ backend/uclapi/oauth/tests.py
@@ -1,6 +1,13 @@
-from django.test import TestCase
+import json
+import unittest.mock
 
-from .models import OAuthScope
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+
+from dashboard.models import App, User
+
+from .decorators import oauth_token_check
+from .models import OAuthScope, OAuthToken
 from .scoping import Scopes
 
 
@@ -74,3 +81,111 @@ class ScopingTestCase(TestCase):
                 "id": 2
             }
         ])
+
+
+class OAuthTokenCheckDecoratorTestCase(TestCase):
+    def setUp(self):
+        mock_status_code = unittest.mock.Mock()
+        mock_status_code.status_code = 200
+        mock_view_func = unittest.mock.Mock(return_value=mock_status_code)
+
+        self.dec_view = oauth_token_check(required_scopes=[])(mock_view_func)
+        self.factory = APIRequestFactory()
+
+    def test_decorator_no_client_secret_proof_provided(self):
+        request = self.factory.get('/')
+        response = self.dec_view(request)
+
+        content = json.loads(response.content.decode())
+
+        self.assertEqual(response.status_code, 400)
+        self.assertFalse(content["ok"])
+        self.assertEqual(content["error"], "No Client Secret Proof provided")
+
+    def test_decorator_no_token_provided(self):
+        request = self.factory.get(
+            '/',
+            {'client_secret_proof': 'not_a_real_proof'}
+        )
+        response = self.dec_view(request)
+
+        content = json.loads(response.content.decode())
+
+        self.assertEqual(response.status_code, 400)
+        self.assertFalse(content["ok"])
+        self.assertEqual(content["error"], "No token provided via GET.")
+
+    def test_decorator_nonexistent_token_provided(self):
+        request = self.factory.get(
+            '/',
+            {'client_secret_proof': 'not_a_real_secret_proof', 'token': 'fake'}
+        )
+        response = self.dec_view(request)
+
+        content = json.loads(response.content.decode())
+
+        self.assertEqual(response.status_code, 400)
+        self.assertFalse(content["ok"])
+        self.assertEqual(content["error"], "Token does not exist")
+
+    def test_decorator_client_secret_verification_failed(self):
+        # create User, App, and OAuth it
+        user_ = User.objects.create(
+            email="test@ucl.ac.uk", cn="test",
+            given_name="Test Test"
+        )
+        app_ = App.objects.create(user=user_, name="An App")
+        oauth_scope = OAuthScope.objects.create()
+        oauth_token = OAuthToken.objects.create(
+            app=app_, user=user_, scope=oauth_scope
+        )
+
+        request = self.factory.get(
+            '/',
+            {
+                'client_secret_proof': 'not_a_real_proof',
+                'token': oauth_token.token
+            }
+        )
+        response = self.dec_view(request)
+
+        content = json.loads(response.content.decode())
+
+        self.assertEqual(response.status_code, 400)
+        self.assertFalse(content["ok"])
+        self.assertEqual(
+            content["error"],
+            "Client secret HMAC verification failed."
+        )
+
+    def test_decorator_no_permission_to_access(self):
+        pass
+
+    def test_decorator_everything_passes(self):
+        user_ = User.objects.create(
+            email="test@ucl.ac.uk", cn="test",
+            given_name="Test Test"
+        )
+        app_ = App.objects.create(user=user_, name="An App")
+        oauth_scope = OAuthScope.objects.create()
+        oauth_token = OAuthToken.objects.create(
+            app=app_, user=user_, scope=oauth_scope
+        )
+
+        # TODO: THIS WILL GET CHANGED
+        # calculate the client_secret_proof
+        import hmac, hashlib, base64
+        hmac_digest = hmac.new(
+            bytes(app_.client_secret, 'ascii'),
+            msg=oauth_token.token.encode('ascii'),
+            digestmod=hashlib.sha256
+        ).digest()
+        hmac_b64 = base64.b64encode(hmac_digest).decode()
+
+        request = self.factory.get(
+            '/',
+            {'client_secret_proof': hmac_b64, 'token': oauth_token.token}
+        )
+        response = self.dec_view(request)
+
+        self.assertEqual(response.status_code, 200)
