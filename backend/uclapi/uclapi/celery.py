b3ef3b761e38e5702b8a73f883d582f9e3bf0996
Christopher Hammond <christopherhammond13@gmail.com>
2017-07-15 15:24:36 +0100

    Adds Opbeat and Sentry support to Celery

--- backend/uclapi/uclapi/celery.py
+++ backend/uclapi/uclapi/celery.py
@@ -1,7 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
+import celery
 import os
-from celery import Celery
+import raven
+
+from django.conf import settings
+from raven.contrib.celery import register_signal, register_logger_signal
+
 from dotenv import read_dotenv
 
 
@@ -9,15 +14,29 @@ read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uclapi.settings')
 
+class Celery(celery.Celery):
+    def on_configure(self):
+        if os.environ.get("SENTRY_DSN") != "":
+            client = raven.Client(os.environ.get("SENTRY_DSN"))
+            register_logger_signal(client)
+            register_signal(client)
+
 app = Celery('uclapi')
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
 app.config_from_object('django.conf.settings', namespace='CELERY')
 
-# app.conf.broker_url = settings.CELERY_BROKER_URL
-app.autodiscover_tasks()
+from opbeat.contrib.django.models import client, logger, register_handlers
+from opbeat.contrib.celery import register_signal as opbeat_register_signal
 
+try:
+    opbeat_register_signal(app)
+except Exception as e:
+    logger.exception('Failed installing celery hook: %s' % e)
+
+if 'opbeat.contrib.django' in settings.INSTALLED_APPS:
+    register_handlers()
+
+app.autodiscover_tasks()
 
 @app.task(bind=True)
 def task(self):
