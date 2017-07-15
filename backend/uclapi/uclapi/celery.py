3ba5f31950440dfeb2f2e17c4c3af97f46bb3daa
Christopher Hammond <christopherhammond13@gmail.com>
2017-07-15 15:34:17 +0100

    Sates Hound

--- backend/uclapi/uclapi/celery.py
+++ backend/uclapi/uclapi/celery.py
@@ -5,7 +5,8 @@ import os
 import raven
 
 from django.conf import settings
-from raven.contrib.celery import register_signal, register_logger_signal
+from raven.contrib.celery import register_signal as raven_register_signal, \
+    register_logger_signal as raven_register_logger_signal
 
 from dotenv import read_dotenv
 
@@ -14,19 +15,24 @@ read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uclapi.settings')
 
+
 class Celery(celery.Celery):
     def on_configure(self):
         if os.environ.get("SENTRY_DSN") != "":
             client = raven.Client(os.environ.get("SENTRY_DSN"))
-            register_logger_signal(client)
-            register_signal(client)
+            raven_register_logger_signal(client)
+            raven_register_signal(client)
+
 
 app = Celery('uclapi')
 
 app.config_from_object('django.conf.settings', namespace='CELERY')
 
-from opbeat.contrib.django.models import client, logger, register_handlers
-from opbeat.contrib.celery import register_signal as opbeat_register_signal
+
+from opbeat.contrib.django.models import \
+    register_handlers as opbeat_register_handlers  # nopep8
+from opbeat.contrib.celery import register_signal as opbeat_register_signal  # nopep8
+
 
 try:
     opbeat_register_signal(app)
@@ -34,9 +40,10 @@ except Exception as e:
     logger.exception('Failed installing celery hook: %s' % e)
 
 if 'opbeat.contrib.django' in settings.INSTALLED_APPS:
-    register_handlers()
+    opbeat_register_handlers()
 
 app.autodiscover_tasks()
+
 
 @app.task(bind=True)
 def task(self):
