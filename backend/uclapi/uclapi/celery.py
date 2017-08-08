929d68771a87a10249bf2e8d3d260cb5331ae8cf
Christopher Hammond <christopherhammond13@gmail.com>
2017-08-08 20:47:29 +0100

    Use a better way of getting the Sentry DSN as per code review

--- backend/uclapi/uclapi/celery.py
+++ backend/uclapi/uclapi/celery.py
@@ -18,7 +18,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uclapi.settings')
 
 class Celery(celery.Celery):
     def on_configure(self):
-        if os.environ.get("SENTRY_DSN") != "":
+        if os.environ.get("SENTRY_DSN") is not None:
             client = raven.Client(os.environ.get("SENTRY_DSN"))
             raven_register_logger_signal(client)
             raven_register_signal(client)
