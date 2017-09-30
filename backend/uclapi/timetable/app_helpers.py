812e818f9c333330679c5d7602b4edbeedff1285
abdulfaizp <abdulfaizp@gmail.com>
2017-09-30 17:35:32 +0100

    first endpoint

--- backend/uclapi/timetable/app_helpers.py
+++ backend/uclapi/timetable/app_helpers.py
@@ -1,12 +1,19 @@
 from roombookings.helpers import PrettyJsonResponse as JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
-from .models import Stumodules, Timetable, Module
+from .models import Stumodules, Timetable, Module, Weekstructure, Weekmapnumeric
 from django.conf import settings
+import datetime
 
 _SETID = settings.ROOMBOOKINGS_SETID
 
+week_map = {}
+
 
 def get_timetable(student):
+    # To avoid hitting database multiple times.
+    if not week_map:
+        _map_weeks()
+
     student_modules = Stumodules.objects.filter(
         setid=_SETID, studentid=student.studentid)
 
@@ -20,7 +27,40 @@ def get_timetable(student):
 
     serialised_timetable = _serialize_timetable(timetable_slots, modules)
 
-# 
-# def _serialize_timetable(timetable_slots, modules):
-#
-#     for slot in timetable_slots:
+
+def _map_weeks():
+    week_nums = Weekmapnumeric.objects.all()
+    week_strs = Weekstructure.objects.all()
+
+    tmp_map = {}
+
+    for wk in week_nums:
+        tmp_map[wk.weekid] = wk.weeknumber
+
+    tmp_map = dict(zip(tmp_map.values(), tmp_map.keys()))
+
+    for wk in week_strs:
+        week_map[tmp_map[wk.weeknumber]] = wk.startdate
+
+
+def _serialize_timetable(timetable_slots, modules):
+    serialized = {}
+    for tt_slot in timetable_slots:
+        date = create_date(tt_slot).isoformat()
+        if date not in serialized:
+            serialized[date] = []
+        serialized[date].append(_get_event(tt_slot))
+
+
+def _get_event(tt_slot):
+    return {
+        "start_time": tt_slot.starttime,
+        "end_time": tt_slot.finishtime,
+        "duration": tt_slot.duration,
+        "module": _get_module_details(tt_slot.moduleid)
+    }
+
+
+def _create_data(tt_slot):
+    return Weekstructure.objects.get(weeknumber=week_map[tt_slot.weekid]) +
+    datetime.timedelta(days=tt_slot.weekday)
