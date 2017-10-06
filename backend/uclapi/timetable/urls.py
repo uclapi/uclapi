5811c60593992966828a576cf09a6de8206f2a45
Wilhelm Klopp <wil.klopp@gmail.com>
2017-10-06 10:49:55 +0100

    Update url naming

--- backend/uclapi/timetable/urls.py
+++ backend/uclapi/timetable/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^personal_timetable$', views.get_personal_timetable),
-    url(r'^module_timeatable$', views.get_modules_timetable),
+    url(r'^personal$', views.get_personal_timetable),
+    url(r'^bymodule$', views.get_modules_timetable),
 ]
