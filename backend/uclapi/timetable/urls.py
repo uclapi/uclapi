d16373609b2f30c6ffa576c1269c529f12c9622c
Christopher Hammond <christopherhammond13@gmail.com>
2017-10-08 01:32:09 +0100

    Switch to fast method for personal timetable

--- backend/uclapi/timetable/urls.py
+++ backend/uclapi/timetable/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^personal_fast$', views.get_personal_timetable_fast),
-    url(r'^personal$', views.get_personal_timetable),
+    url(r'^personal$', views.get_personal_timetable_fast),
     url(r'^bymodule$', views.get_modules_timetable),
 ]
