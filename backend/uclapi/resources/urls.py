de06da81e9f301e323619ff9a7d5b0a5d1bf4883
Christopher Hammond <christopherhammond13@gmail.com>
2017-11-02 23:10:59 +0000

    Renames endpoint to /resources/desktops for future expansion

--- backend/uclapi/resources/urls.py
+++ backend/uclapi/resources/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^desktop_availability$', views.get_pc_availability),
+    url(r'^desktops$', views.get_pc_availability),
 ]
