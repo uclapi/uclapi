1b6a8dd0361f69c67ab1f910295a17f04ee12131
Marco Concetto Rudilosso <marcoconcettorudilosso@gmail.com>
2017-12-09 15:43:02 +0000

    not overwriting the .env

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,18 +2,6 @@ ORACLE_VERSION=12_2
 ORACLE_SO_VERSION=12.1
 export ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION
 
-sed "s/DB_UCLAPI_HOST=.*/DB_UCLAPI_HOST=$DB_UCLAPI_HOST/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env 
-sed "s/DB_UCLAPI_USERNAME=.*/DB_UCLAPI_USERNAME=$DB_UCLAPI_USERNAME/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env
-sed "s/DB_UCLAPI_PASSWORD=.*/DB_UCLAPI_PASSWORD=$DB_UCLAPI_PASSWORD/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env
-sed "s/DB_CACHE_HOST=.*/DB_CACHE_HOST=$DB_CACHE_HOST/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env
-sed "s/DB_CACHE_USERNAME=.*/DB_CACHE_USERNAME=$DB_CACHE_USERNAME/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env
-sed "s/DB_CACHE_PASSWORD=.*/DB_CACHE_PASSWORD=$DB_CACHE_PASSWORD/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env
-sed "s/REDIS_UCLAPI_HOST=.*/REDIS_UCLAPI_HOST=$REDIS_UCLAPI_HOST/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env
-sed "s/PYTHONUNBUFFERED=.*/PYTHONUNBUFFERED=$PYTHONUNBUFFERED/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env
-sed "s/ORACLE_HOME=.*/ORACLE_HOME=$ORACLE_HOME/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env
-sed "s/DB_UCLAPI_NAME=.*/DB_UCLAPI_NAME=$DB_UCLAPI_NAME/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env
-sed "s/DB_CACHE_NAME=.*/DB_CACHE_NAME=$DB_CACHE_NAME/" ./backend/uclapi/.env > temp && mv temp ./backend/uclapi/.env
-
 cd backend/uclapi
 virtualenv --python=python3 venv
 . venv/bin/activate
