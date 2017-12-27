4eed7166c0521ae8d82a3d0442580ecf9efb6edd
Anirudh <anirudhx5@gmail.com>
2017-12-27 14:48:28 +0000

    404 page

--- frontend/src/react/components/marketplace/AppPage/AppPage.jsx
+++ frontend/src/react/components/marketplace/AppPage/AppPage.jsx
@@ -5,6 +5,7 @@ import allApps from './../allApps.jsx';
 import Header from './Header.jsx';
 import Left from './Left.jsx';
 import Right from './Right.jsx';
+import NotFound from './NotFound.jsx';
 
 
 export default class AppPageComponent extends React.Component {
@@ -12,29 +13,42 @@ export default class AppPageComponent extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      app: allApps[this.props.appId]
+    if (this.props.appId in allApps) {
+      this.state = {
+        found: true,
+        app: allApps[this.props.appId],
+      }
     }
+    else {
+      this.state = {
+        found: false,
+      }
+    }
+
   }
 
   render() {
     return (
-      <div className="appPage">
-        <div className="container">
-          <Header app={this.state.app} />
+      (this.state.found) ? (
+        <div className="appPage">
+          <div className="container">
+            <Header app={this.state.app} />
 
-          <Paper className="paper" zDepth={1}>
-            <div className="row">
-              <div className="col3">
-                <Left app={this.state.app} />
+            <Paper className="paper" zDepth={1}>
+              <div className="row">
+                <div className="col3">
+                  <Left app={this.state.app} />
+                </div>
+                <div className="col9">
+                  <Right app={this.state.app} />
+                </div>
               </div>
-              <div className="col9">
-                <Right app={this.state.app} />
-              </div>
-            </div>
-          </Paper>
+            </Paper>
+          </div>
         </div>
-      </div>
+      ): (
+        <NotFound />
+      )
     )
   }
 
