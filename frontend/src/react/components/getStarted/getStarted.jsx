a71add682afd9050d676bb5bbed673c5920196d6
Anirudh <anirudhx5@gmail.com>
2018-01-12 22:15:21 +0000

    added marketplace

--- frontend/src/react/components/getStarted/getStarted.jsx
+++ frontend/src/react/components/getStarted/getStarted.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Navbar from './Navbar.jsx';
 import Intro from './Intro.jsx';
 import Goal from './Goal.jsx';
 import Demo from './Demo.jsx';
+import Marketplace from './Marketplace.jsx';
 import Footer from './Footer.jsx';
 
 
@@ -12,10 +12,10 @@ export default class GetStartedComponent extends React.Component {
     render () {
       return (
         <div>
-          <Navbar />
           <Intro />
           <Goal />
           <Demo />
+          <Marketplace />
           <Footer />
         </div>
       )
