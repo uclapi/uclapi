75f61563567802985f09d5dbbe4c3167059aac31
Christopher Hammond <christopherhammond13@gmail.com>
2018-02-27 14:17:25 +0000

    Removes rogue hyphen from URL

--- backend/uclapi/workspaces/urls.py
+++ backend/uclapi/workspaces/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
      url(r'^surveys$', views.get_surveys),
      url(r'^image$', views.get_image),
-     url(r'^sensors/last-updated$', views.get_survey_max_timestamp),
+     url(r'^sensors/lastupdated$', views.get_survey_max_timestamp),
      url(r'^sensors/summary$', views.get_survey_sensors_summary),
      url(r'^sensors$', views.get_survey_sensors),
 ]
