ceae285f9d2813bcc8858699dcf449df72d5628b
Christopher Hammond <christopherhammond13@gmail.com>
2018-03-05 17:19:52 +0000

    Fixes gunicorn multithreading and updates requirements

--- backend/uclapi/gunicorn_config.py
+++ backend/uclapi/gunicorn_config.py
@@ -2,12 +2,15 @@ import multiprocessing
 
 bind = "127.0.0.1:9000"
 
-# Run cores * 4 + 1 workers in gunicorn
-# This is set deliberately high in case of long Oracle transactions locking Django up
-workers = multiprocessing.cpu_count() * 4 + 1
-threads = multiprocessing.cpu_count() * 4
+# The recommended number of workers is 2 * cores + 1
+workers = multiprocessing.cpu_count() * 2 + 1
 
-worker_class = "sync"
+# Ideally one thread should be enough, but this is scaled
+# slightly due to the long, blocking calls some of the
+# endpoints make
+threads = multiprocessing.cpu_count() * 2
+
+worker_class = "gaiohttp"
 
 daemon = False
 
