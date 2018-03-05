d2face03faaa9f7f61189d39aac28ba43d50d1d1
Christopher Hammond <christopherhammond13@gmail.com>
2018-03-05 18:07:21 +0000

    Switch to eventlet

--- backend/uclapi/gunicorn_config.py
+++ backend/uclapi/gunicorn_config.py
@@ -5,12 +5,7 @@ bind = "127.0.0.1:9000"
 # The recommended number of workers is 2 * cores + 1
 workers = multiprocessing.cpu_count() * 2 + 1
 
-# Ideally one thread should be enough, but this is scaled
-# slightly due to the long, blocking calls some of the
-# endpoints make
-threads = multiprocessing.cpu_count() * 2
-
-worker_class = "gaiohttp"
+worker_class = "eventlet"
 
 daemon = False
 
