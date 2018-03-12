f2968789b1f4f6b8ff90fe259ddc5dc2cf2a1f70
Christopher Hammond <christopherhammond13@gmail.com>
2018-03-12 22:28:07 +0000

    Fixes a minor tdocs typo

--- frontend/src/react/components/documentation/Routes/OAuth/StudentNumber.jsx
+++ frontend/src/react/components/documentation/Routes/OAuth/StudentNumber.jsx
@@ -51,7 +51,7 @@ export default class StudentNumber extends React.Component {
               Endpoint: <code>https://uclapi.com/oauth/user/studentnumber</code>
             </p>
             <p>
-              You can use the `oauth/user/data` endpoint to find out whether the user is a student before you call this endpoint. If you call this endpoint and the user is not a student, an error will be returned.
+              You can use the <code>oauth/user/data</code> endpoint to find out whether the user is a student before you call this endpoint. If you call this endpoint and the user is not a student, an error will be returned.
             </p>
             <p>
               Please note: to use this endpoint you must have ticked the <em>Student Number</em> scope for your application in the UCL API Dashboard. This piece of information has been separated from the others because a student number can in some cases be considered confidential. This is because any data exported directly from Portico, SITS (E:Vision) or CMIS is usually grouped by Student Number. One example is that in some cases departments choose to release spreadsheets of examination results where each student is identified by their student number, and not their name, to provide a degree of anonymity in what is otherwise an open data set. You should consider carefully whether you actually need a student number to track students when other unique identifiers are available, such as their username-based email address and UPI. If you request a student number and it is not required for your application, your users may choose not to provide this information to you, and therefore deny your application permission to access their details.
