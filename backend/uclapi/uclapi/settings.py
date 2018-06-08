ddd3441d05eeb3dd9455831b3c85a7d18b2fadfa
Anirudh <anirudhx5@gmail.com>
2018-06-08 15:24:14 +0100

    Fixed JS code example on Homepage

--- backend/uclapi/uclapi/settings.py
+++ backend/uclapi/uclapi/settings.py
@@ -194,7 +194,7 @@ WEBPACK_SERVE = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://localhost:8080/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Cross Origin settings
