9d9c949863bd49d23585e52992ebd339bcb95d8e
Zak Morgan <iiianmmm@gmail.com>
2019-01-27 17:24:21 +0000

    hound

--- backend/uclapi/dashboard/app_helpers.py
+++ backend/uclapi/dashboard/app_helpers.py
@@ -13,7 +13,7 @@ import validators
 def get_articles():
     r = redis.Redis(host=REDIS_UCLAPI_HOST)
     articles = [["", ""], ["", ""], ["", ""]]
-    for i in range(0 ,3):
+    for i in range(0, 3):
         articles[i][0] = r.get("Blog:item"+str(i)+":url").decode("utf-8")
         articles[i][1] = r.get("Blog:item"+str(i)+":title").decode("utf-8") 
     return articles
