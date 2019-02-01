14749cf9c1edfe4b75045658d299c70f73ed3fd1
TuxedoFish <harryliversedge@gmail.com>
2019-02-01 13:27:35 +0000

    was double wrapped in a pure-g

--- frontend/src/components/appsettings/loginbutton.jsx
+++ frontend/src/components/appsettings/loginbutton.jsx
@@ -3,23 +3,22 @@ import PropTypes from 'prop-types';
 
 class LogInButton extends React.Component {
   render () {
-    return <div class="pure-g">
+    return
     <div class="pure-u-md-1-4"></div>
-    <div class="pure-u-1 pure-u-md-1-2 centered">
-      <h1>Log in to handle app settings</h1>
-      <div class="card flexCentre">
-        <form class="pure-form pure-form-stacked" action="/oauth/appsettings" method="post">
-          <fieldset>
-            <div class="well left">
-            </div>
-            <input id="remember" name="agreement" value="True" hidden></input>
-            <button type="submit" class="pure-button pure-button-primary">Log In</button>
-          </fieldset>
-        </form>
+      <div class="pure-u-1 pure-u-md-1-2 centered">
+        <h1>Log in to handle app settings</h1>
+        <div class="card flexCentre">
+          <form class="pure-form pure-form-stacked" action="/oauth/appsettings" method="post">
+            <fieldset>
+              <div class="well left">
+              </div>
+              //<input id="remember" name="agreement" value="True" hidden></input>
+              <button type="submit" class="pure-button pure-button-primary">Log In</button>
+            </fieldset>
+          </form>
+        </div>
       </div>
-    </div>
-    <div class="pure-u-md-1-4"></div>
-  </div>;
+    <div class="pure-u-md-1-4"></div>;
   }
 }
 
