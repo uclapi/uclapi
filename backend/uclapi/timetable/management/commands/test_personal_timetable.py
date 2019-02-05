51adf4ab9c96df32fe315388c56468f21c0848b4
Alexander Saoutkin <a.saoutkin@gmail.com>
2019-02-05 15:08:02 +0000

    Changed output from single to double quotes

--- backend/uclapi/timetable/management/commands/test_personal_timetable.py
+++ backend/uclapi/timetable/management/commands/test_personal_timetable.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+import json
 
 from timetable.app_helpers import get_student_timetable
 
@@ -10,4 +11,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         upi = input("Please enter the student UPI: ")
         tt = get_student_timetable(upi)
-        print(tt)
+        print(json.dumps(tt))
