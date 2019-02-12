a619644484938a6c5d1c699643805c1574ea0b49
Christopher Hammond <christopherhammond13@gmail.com>
2019-02-12 02:26:42 +0000

    Go away Hound, they're long strings!

--- backend/uclapi/common/management/commands/dev_environment_setup.py
+++ backend/uclapi/common/management/commands/dev_environment_setup.py
@@ -38,9 +38,9 @@ class Command(BaseCommand):
             app = App(
                 user=user,
                 name="Local OAuth Test",
-                api_token='uclapi-4286bc18b235d86-ab0998cc3a47a9b-07b6dfe234a04bf-97407a655b33ae8',
+                api_token='uclapi-4286bc18b235d86-ab0998cc3a47a9b-07b6dfe234a04bf-97407a655b33ae8',  # noqa
                 client_id='1105308584328350.9460393713696551',
-                client_secret='251e9f9553bb3b86829c18bf795844d977dedf569b24a70e4d4e753958fcc2f3',
+                client_secret='251e9f9553bb3b86829c18bf795844d977dedf569b24a70e4d4e753958fcc2f3',    # noqa
                 callback_url='http://localhost:8002/uclapi/callback'
             )
             app.save()
