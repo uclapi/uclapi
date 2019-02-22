763cc13d81a09f42d0cba48b4806aef4cab92c51
Zak Morgan <iiianmmm@gmail.com>
2019-02-22 00:22:03 +0000

    fixes spacing

--- frontend/src/components/getStarted/QandA.jsx
+++ frontend/src/components/getStarted/QandA.jsx
@@ -4,11 +4,11 @@ import Collapse, { Panel } from 'rc-collapse';
 export default class QandA extends React.Component {
     render() {
     	return (
-		<Collapse>
-    	  <Panel header={this.props.question} showArrow>
-    	    <p>{this.props.answer}</p>
-    	  </Panel>
-	  	</Collapse>
+	  <Collapse>
+    	    <Panel header={this.props.question} showArrow>
+    	      <p>{this.props.answer}</p>
+    	    </Panel>
+	  </Collapse>
     	);
-	}
+    }
 }
