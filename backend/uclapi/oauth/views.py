2356ce5649a87d6c92893e1423aa7e5baf53cac6
Harry M Liversedge <liversedge@LAPTOP-1TVSG7CF.localdomain>
2019-03-11 14:34:52 +0000

    Finished updating templates

--- backend/uclapi/oauth/views.py
+++ backend/uclapi/oauth/views.py
@@ -637,6 +637,7 @@ def my_apps(request):
     scopes = Scopes()
 
     for token in tokens:
+        print(token)
         authorised_apps.append({
             "id": token.id,
             "active": token.active,
@@ -654,6 +655,7 @@ def my_apps(request):
     initial_data_dict = {
         "status" : "ONLINE",
         "fullname": user.full_name,
+        "user_id": user.id,
         "department": user.department,
         "scopes": scopes.get_scope_map(),
         "apps": authorised_apps
