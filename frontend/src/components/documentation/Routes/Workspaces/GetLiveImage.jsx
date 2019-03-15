0bb4a13711f3f653dd9651929a480ff25cd35665
Alexander Saoutkin <a.saoutkin@gmail.com>
2019-03-15 14:18:55 +0000

    Changing example to give valid parameters

--- frontend/src/components/documentation/Routes/Workspaces/GetLiveImage.jsx
+++ frontend/src/components/documentation/Routes/Workspaces/GetLiveImage.jsx
@@ -10,7 +10,7 @@ let codeExamples = {
 params = {
   "token": "uclapi-5d58c3c4e6bf9c-c2910ad3b6e054-7ef60f44f1c14f-a05147bfd17fdb",
   "survey_id": "22",
-  "map_id": "3"
+  "map_id": "84"
 }
 
 r = requests.get("https://uclapi.com/workspaces/images/map/live", params=params)
