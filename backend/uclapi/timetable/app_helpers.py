eacf2ff1dc06b5083de8e6e611c111fd0f7462aa
Alexander Saoutkin <a.saoutkin@gmail.com>
2019-03-31 16:11:44 +0100

    Fixing duplication of instances

--- backend/uclapi/timetable/app_helpers.py
+++ backend/uclapi/timetable/app_helpers.py
@@ -239,6 +239,11 @@ def _get_timetable_events(full_modules):
     timetable = get_cache("timetable")
 
     bookings = get_cache("booking")
+    distinct_fields = (
+        'setid', 'siteid', 'roomid', 'sitename', 'roomname', 'bookabletype',
+        'slotid', 'bookingid', 'starttime', 'finishtime', 'startdatetime',
+        'finishdatetime', 'weeknumber', 'condisplayname', 'phone', 'descrip'
+    )
     event_bookings_list = {}
     full_timetable = {}
     modules_chosen = {}
@@ -261,7 +266,7 @@ def _get_timetable_events(full_modules):
         for event in events_data:
             if event.slotid not in event_bookings_list:
                 event_bookings_list[event.slotid] =  \
-                    bookings.objects.filter(slotid=event.slotid)
+                        bookings.objects.filter(slotid=event.slotid, title=module.name).distinct(*distinct_fields)
             event_bookings = event_bookings_list[event.slotid]
             # .exists() instead of len so we don't evaluate all of the filter
             if not event_bookings.exists():
