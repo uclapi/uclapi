7c197370b43ea8c76939e3ef361448dbbf1164d1
zipy124 <iiianmmm@gmail.com>
2019-05-23 23:19:40 +0100

    Standardize formatting

--- backend/uclapi/workspaces/occupeye/constants.py
+++ backend/uclapi/workspaces/occupeye/constants.py
@@ -90,19 +90,39 @@ class OccupEyeConstants():
     SURVEY_LOCATIONS = {
         "UCL Torrington 1-19 113": {
          "lat": "51.521886", "long": "-0.134415",
-         "address": ["1-19 Torrington Pl", "Fitzrovia", "London", "WC1E 7HB"]
+         "address": [
+            "1-19 Torrington Pl",
+            "Fitzrovia",
+            "London",
+            "WC1E 7HB"
+         ]
         },
         "UCL Student Centre": {
          "lat": "51.524916", "long": "-0.132371",
-         "address": ["27-28 Gordon Square", "Bloomsbury", "London", "WC1H 0AH"]
+         "address": [
+            "27-28 Gordon Square",
+            "Bloomsbury",
+            "London",
+            "WC1H 0AH"
+         ]
         },
         "UCL SSEES Library": {
          "lat": "51.525342", "long": "-0.131602",
-         "address": ["16 Taviton St", "Bloomsbury", "London", "WC1H 0BW"]
+         "address": [
+            "16 Taviton St",
+            "Bloomsbury",
+            "London",
+            "WC1H 0BW"
+         ]
         },
         "UCL SENIT Suite": {
          "lat": "51.524280", "long": "-0.133026",
-         "address": ["Senit Suite", "Kings Cross", "London", "WC1E 6BT"]
+         "address": [
+            "Senit Suite",
+            "Kings Cross",
+            "London",
+            "WC1E 6BT"
+         ]
         },
         "UCL Senate House Hub": {
          "lat": "51.521094", "long": "-0.128735",
@@ -115,7 +135,12 @@ class OccupEyeConstants():
         },
         "UCL Science Library": {
          "lat": "51.523556", "long": "-0.132588",
-         "address": ["Malet Pl", "Bloomsbury", "London", "WC1E"]
+         "address": [
+            "Malet Pl",
+            "Bloomsbury",
+            "London",
+            "WC1E"
+         ]
         },
         "UCL School of Pharmacy Library": {
          "lat": "51.524967", "long": "-0.122872",
@@ -128,11 +153,21 @@ class OccupEyeConstants():
         },
         "UCL Main Library": {
          "lat": "51.524748", "long": "-0.133535",
-         "address": ["Gower St", "Bloomsbury", "London", "WC1E 6BT"]
+         "address": [
+            "Gower St",
+            "Bloomsbury",
+            "London",
+            "WC1E 6BT"
+         ]
         },
         "UCL Language & Speech Science Library": {
          "lat": "51.525847", "long": "-0.122785",
-         "address": ["2 Wakefield St", "Kings Cross", "London", "WC1N 1PJ"]
+         "address": [
+            "2 Wakefield St",
+            "Kings Cross",
+            "London",
+            "WC1N 1PJ"
+         ]
         },
         "UCL Institute of Orthopaedics Library": {
          "lat": "51.631529", "long": "-0.306551",
@@ -145,7 +180,12 @@ class OccupEyeConstants():
         },
         "UCL Institute of Neurology, Queen Square Library": {
          "lat": "51.522398", "long": "-0.122973",
-         "address": ["23 Queen Square", "London", "WC1N 3BG", ""]
+         "address": [
+            "23 Queen Square",
+            "London",
+            "WC1N 3BG",
+            ""
+         ]
         },
         "UCL Institute of Education Library": {
          "lat": "51.522897", "long": "-0.127864",
@@ -194,31 +234,66 @@ class OccupEyeConstants():
         },
         "UCL Cruciform Hub": {
          "lat": "51.524054", "long": "-0.135032",
-         "address": ["Gower St", "Fitzrovia", "London", "WC1E 6BT"]
+         "address": [
+            "Gower St",
+            "Fitzrovia",
+            "London",
+            "WC1E 6BT"
+         ]
         },
         "UCL Christopher Ingold G20": {
          "lat": "51.525370", "long": "-0.132171",
-         "address": ["20 Gordon St", "Kings Cross", "London", "WC1H 0AJ"]
+         "address": [
+            "20 Gordon St",
+            "Kings Cross",
+            "London",
+            "WC1H 0AJ"
+         ]
         },
         "UCL chadwick B04": {
          "lat": "51.524233", "long": "-0.134336",
-         "address": ["Gower St", "Bloomsbury", "London", "WC1E 6DE"]
+         "address": [
+            "Gower St",
+            "Bloomsbury",
+            "London",
+            "WC1E 6DE"
+         ]
         },
         "UCL Chadwick 223": {
          "lat": "51.524233", "long": "-0.134336",
-         "address": ["Gower St", "Bloomsbury", "London", "WC1E 6DE"]
+         "address": [
+            "Gower St",
+            "Bloomsbury",
+            "London",
+            "WC1E 6DE"
+         ]
         },
         "UCL Bedfordway G11": {
          "lat": "51.523857", "long": "-0.128845",
-         "address": ["26 Bedford Way", "Bloomsbury", "London", "WC1H 0AP"]
+         "address": [
+            "26 Bedford Way",
+            "Bloomsbury",
+            "London",
+            "WC1H 0AP"
+         ]
         },
         "UCL Bedfordway 316": {
          "lat": "51.523857", "long": "-0.128845",
-         "address": ["26 Bedford Way", "Bloomsbury", "London", "WC1H 0AP"]
+         "address": [
+            "26 Bedford Way",
+            "Bloomsbury",
+            "London",
+            "WC1H 0AP"
+         ]
         },
         "UCL Bartlett Library": {
          "lat": "51.526812", "long": "-0.129855",
-         "address": ["14 Upper Woburn Pl", "Bloomsbury", "London", "WC1H 0NN"]
+         "address": [
+            "14 Upper Woburn Pl",
+            "Bloomsbury",
+            "London",
+            "WC1H 0NN"
+         ]
         },
         "UCL Archaeology 501": {
          "lat": "51.524908", "long": "-0.131575",
@@ -249,7 +324,12 @@ class OccupEyeConstants():
         },
         "UCL 23 Gordon Square  history cluster": {
          "lat": "51.524497", "long": "-0.132115",
-         "address": ["23 GORDON SQUARE", "LONDON", "WC1H 0AG", ""]
+         "address": [
+            "23 GORDON SQUARE",
+            "LONDON",
+            "WC1H 0AG",
+            ""
+         ]
         },
         "The Joint Library of Ophthalmology": {
          "lat": "51.527235", "long": "-0.091205",
@@ -262,22 +342,47 @@ class OccupEyeConstants():
         },
         "Royal Free Hospital Medical Library": {
          "lat": "51.552507", "long": "-0.165783",
-         "address": ["Rowland Hill Street", "Hampstead", "London", "NW3 2PF"]
+         "address": [
+            "Rowland Hill Street",
+            "Hampstead",
+            "London",
+            "NW3 2PF"
+         ]
         },
         "Graduate Hub - RP Dev Testing": {
          "lat": "51.524143", "long": "-0.133525",
-         "address": ["Kings Cross", "London", "WC1E 6DE", ""]
+         "address": [
+            "Kings Cross",
+            "London",
+            "WC1E 6DE",
+            ""
+         ]
         },
         "Graduate Hub": {
          "lat": "51.524143", "long": "-0.133525",
-         "address": ["Kings Cross", "London", "WC1E 6DE", ""]
+         "address": [
+            "Kings Cross",
+            "London",
+            "WC1E 6DE",
+            ""
+         ]
         },
         "Foster Court": {
          "lat": "51.523555", "long": "-0.132497",
-         "address": ["Gower St", "Bloomsbury", "London", "WC1E 6BT"]
+         "address": [
+            "Gower St",
+            "Bloomsbury",
+            "London",
+            "WC1E 6BT"
+         ]
         },
         "1 Saint Martin Le Grand": {
          "lat": "51.516380", "long": "-0.097666",
-         "address": ["1 St Martin's Le Grand", "London", "EC1A 4NP", ""]
+         "address": [
+            "1 St Martin's Le Grand",
+            "London",
+            "EC1A 4NP",
+            ""
+         ]
         }
     }
