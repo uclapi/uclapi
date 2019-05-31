fd196fb9de7a45663b734a05372478c00477df1a
Harry M Liversedge <liversedge@LAPTOP-1TVSG7CF.localdomain>
2019-05-31 07:30:40 +0100

    MERGE added in scss styling

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import Navbar from '../components/about/Navbar.jsx';
 import Wil from '../components/about/Wil.jsx';
 import PersonContainer from '../components/about/PersonContainer.jsx';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import './../sass/about.scss';
 
  export default class AboutComponent extends React.Component {
 
