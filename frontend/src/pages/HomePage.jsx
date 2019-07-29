703de324feb8a51161a928405050a9a242260214
Harry Liversedge <Harry.Liversedge@tesco.com>
2019-07-29 18:50:53 +0100

    Had an issue with react syntax highlighter whilst refactoring so bumped it up and updated docs

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,48 +1,28 @@
+// Standard React importss
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {cyan500, cyan700,
-  pinkA200,
-  grey100, grey300, grey400, grey500,
-  white, darkBlack, fullBlack,
-} from 'material-ui/styles/colors';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import GetStartedComponent from '../components/getStarted/getStarted.jsx';
 
+// Styles
 import './../sass/getStarted.scss';
 import './../sass/navbar.scss';
 
-const muiTheme = getMuiTheme({
-  fontFamily: 'Roboto, sans-serif',
-  palette: {
-    primary1Color: "#434343",
-    primary2Color: "#434343",
-    primary3Color: grey400,
-    accent1Color: pinkA200,
-    accent2Color: grey100,
-    accent3Color: grey500,
-    textColor: darkBlack,
-    alternateTextColor: white,
-    canvasColor: white,
-    borderColor: grey300,
-    pickerHeaderColor: cyan500,
-    shadowColor: fullBlack,
-  },
-});
+// Common Components
+import SingleCentralColumn from '../components/layout/container/SingleCentralColumn.jsx';
 
-class GetStarted extends React.Component {
+class HomePage extends React.Component {
 
   render () {
     return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <GetStartedComponent />
-      </MuiThemeProvider>
-    )
+      <div className = "home-page">
+        <SingleCentralColumn 
+          child = "" />
+      </div>
+    );
   }
 
 }
 
 ReactDOM.render(
-  <GetStarted />,
+  <HomePages />,
   document.querySelector('.app')
 );
