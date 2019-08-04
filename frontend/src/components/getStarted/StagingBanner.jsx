38c06ada024d194f8be3b88938614658ea4ce512
zipy124 <iiianmmm@gmail.com>
2019-08-04 16:31:41 +0100

    Banner link in Header block

--- frontend/src/components/getStarted/StagingBanner.jsx
+++ frontend/src/components/getStarted/StagingBanner.jsx
@@ -7,10 +7,11 @@ export default class StagingBanner extends React.Component {
     return (
       <div className="stagingBanner">
         <div className="container">
-          <h1>Warning! This is our bleeding-edge staging environment, and therefore performance, accuracy and reliability of the API cannot be guaranteed. For our stable, supported API please go to</h1>
+          <h1>Warning! This is our bleeding-edge staging environment, and therefore performance, accuracy and reliability of the API cannot be guaranteed. For our stable, supported API please go to
           <a href={"https://uclapi.com"}>
             uclapi.com
           </a>
+          </h1>
         </div>
       </div>
     )
