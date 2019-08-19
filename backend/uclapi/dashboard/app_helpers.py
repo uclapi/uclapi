66ab65ed2a5001053b0b2cb801f6e84a6d5b6635
Harry Liversedge <Harry.Liversedge@tesco.com>
2019-08-19 14:34:08 +0100

    ...

--- backend/uclapi/dashboard/app_helpers.py
+++ backend/uclapi/dashboard/app_helpers.py
@@ -51,23 +51,16 @@ def get_articles():
     redis_response = pipe.execute()
     for i in range(0, MEDIUM_ARTICLE_QUANTITY):
         start_index = i*8
+        type = "utf-8"
 
-        articles[i]['title'] = 
-            redis_response[start_index].decode("utf-8")
-        articles[i]['url'] = 
-            redis_response[start_index+1].decode("utf-8")
-        articles[i]['tags'] = 
-            redis_response[start_index+2].decode("utf-8")
-        articles[i]['creator'] = 
-            redis_response[start_index+3].decode("utf-8")
-        articles[i]['published'] = 
-            redis_response[start_index+4].decode("utf-8")
-        articles[i]['updated'] = 
-            redis_response[start_index+5].decode("utf-8")
-        articles[i]['content'] = 
-            redis_response[start_index+6].decode("utf-8")
-        articles[i]['image_url'] = 
-            redis_response[start_index+7].decode("utf-8")
+        articles[i]['title'] = redis_response[start_index].decode(type)
+        articles[i]['url'] = redis_response[start_index+1].decode(type)
+        articles[i]['tags'] = redis_response[start_index+2].decode(type)
+        articles[i]['creator'] = redis_response[start_index+3].decode(type)
+        articles[i]['published'] = redis_response[start_index+4].decode(type)
+        articles[i]['updated'] = redis_response[start_index+5].decode(type)
+        articles[i]['content'] = redis_response[start_index+6].decode(type)
+        articles[i]['image_url'] = redis_response[start_index+7].decode(type)
     return articles
 
 
