ee15d841630cbf00570960982bcbd77cb930b29b
zipy124 <iiianmmm@gmail.com>
2019-08-20 14:12:11 +0100

    deployment script update

--- deployment_staging.sh
+++ deployment_staging.sh
@@ -12,5 +12,5 @@ cd ../backend/uclapi
 deactivate
 mv .env .env.staging
 mv .env.dev .env
-ssh -i staging.pem ubuntu@52.56.191.73 -t "cd ~; ./deploy.sh;"
-ssh -J ubuntu@52.56.191.73 -i staging.pem ubuntu@ip-10-0-2-138.eu-west-2.compute.internal -v -t "cd ~; ./deploy.sh;"
+ssh -i ~/.ssh/staging.pem ubuntu@52.56.191.73 -t "cd ~; ./deploy.sh;"
+ssh -J ubuntu@52.56.191.73 -i ~/.ssh/staging.pem ubuntu@ip-10-0-2-138.eu-west-2.compute.internal -v -t "cd ~; ./deploy.sh;"
