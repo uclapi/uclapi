6f93bb3a5ad71c90abab9682a221e06ac437a53a
zipy124 <iiianmmm@gmail.com>
2019-08-20 14:34:16 +0100

    prod

--- deployment_production.sh
+++ deployment_production.sh
@@ -12,4 +12,5 @@ cd ../backend/uclapi
 deactivate
 mv .env .env.prod
 mv .env.dev .env
-
+ssh -i ~/.ssh/prod.pem ubuntu@ec2-35-176-91-93.eu-west-2.compute.amazonaws.com -t "cd ~; ./deploy.sh;"
+ssh -J ubuntu@ec2-35-176-91-93.eu-west-2.compute.amazonaws.com -i ~/.ssh/staging ubuntu@ip-10-0-1-78.eu-west-2.compute.internal -v -t "cd ~; ./deploy.sh;"
