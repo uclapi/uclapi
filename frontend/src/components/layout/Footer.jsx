0dfcd7b7773618e2288b673190201ab4179ce0b1
TuxedoFish <harryliversedge@gmail.com>
2019-10-30 15:13:34 +0000

    Sorted out the colors

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -21,18 +21,18 @@ export default class Footer extends React.Component {
     var logosize = '20px';
 
     return (
-      <Row color='secondary' padding='20px 0'>
+      <Row styling='secondary' style={ { padding : '20px 0'} }>
           <Column width='1-2' horizontalAlignment='center'>
               <Column maxWidth='160px' width='1-1' horizontalAlignment='center'>
-                <CardView width='1-3' style='github' link='https://github.com/uclapi'>
+                <CardView width='1-3' type='github' link='https://github.com/uclapi'>
                   <ImageView src={github} width={logosize} height={logosize} description={'github logo'} isCentered={true} />
                 </CardView>
 
-                <CardView width='1-3' style='facebook' link='https://www.facebook.com/uclapi/'>
+                <CardView width='1-3' type='facebook' link='https://www.facebook.com/uclapi/'>
                   <ImageView src={facebook} width={logosize} height={logosize} description={'facebook logo'} isCentered={true} />
                 </CardView>
 
-                <CardView width='1-3' style='twitter' link='https://twitter.com/uclapi'>
+                <CardView width='1-3' type='twitter' link='https://twitter.com/uclapi'>
                   <ImageView src={twitter} width={logosize} height={logosize} description={'twitter logo'} isCentered={true} />
                 </CardView>
               </Column>
