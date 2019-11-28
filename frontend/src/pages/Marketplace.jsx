cb69ea3de885c8a1af118e4a45695e339e0659e8
Huey <leejinhuey@gmail.com>
2019-11-28 16:23:56 +0000

    address eslint warnings

--- frontend/src/pages/Marketplace.jsx
+++ frontend/src/pages/Marketplace.jsx
@@ -15,7 +15,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 // Grab titles and descriptions of app
 import { allApps } from 'Layout/data/app_pages.jsx'
 // Common Components
-import { ButtonView, CardView, Column, Footer, ImageView, NavBar, Row, TextView } from 'Layout/Items.jsx'
+import {
+  ButtonView, CardView, Column, Footer, ImageView, NavBar, Row, TextView,
+} from 'Layout/Items.jsx'
 
 class Marketplace extends React.Component {
 
@@ -54,26 +56,60 @@ class Marketplace extends React.Component {
 
         <NavBar isScroll={false} />
 
-        <Row height='300px' style={{ margin: `60px 0 0 0` }} styling='splash-parallax'>
-          <Column width='2-3' horizontalAlignment='center' verticalAlignment='center'>
+        <Row
+          height='300px'
+          style={{ margin: `60px 0 0 0` }}
+          styling='splash-parallax'
+        >
+          <Column
+            width='2-3'
+            horizontalAlignment='center'
+            verticalAlignment='center'
+          >
             <TextView text={`UCL Marketplace`} heading={1} align={`center`} />
-            <TextView text={`Apps that use UCL API`} heading={2} align={`center`} />
+            <TextView
+              text={`Apps that use UCL API`}
+              heading={2}
+              align={`center`}
+            />
           </Column>
         </Row>
 
         <Row styling='secondary'>
           <Column width='2-3' horizontalAlignment='center'>
             <TextView text={`Featured App`} heading={2} align={`left`} />
-            <TextView text={`Our favourite usage of the API`} heading={5} align={`left`} />
+            <TextView
+              text={`Our favourite usage of the API`}
+              heading={5}
+              align={`left`}
+            />
             {featuredApps.map((app, i) => {
               return (
-                <CardView key={`featured-app-` + i} width={`1-1`} type={`emphasis`} link={`/marketplace/` + app.id}
+                <CardView
+                  key={`featured-app-` + i}
+                  width={`1-1`}
+                  type={`emphasis`}
+                  link={`/marketplace/` + app.id}
                   style={{ 'padding': `20px 0 ` }}
                 >
                   <Column width='1-2' horizontalAlignment='center'>
-                    <ImageView src={app.logolight} width={iconsize} height={iconsize} />
-                    <TextView text={app.name} heading={2} align={`center`} color={`white`} />
-                    <TextView text={app.description} heading={5} align={`center`} color={`white`} />
+                    <ImageView
+                      src={app.logolight}
+                      width={iconsize}
+                      height={iconsize}
+                    />
+                    <TextView
+                      text={app.name}
+                      heading={2}
+                      align={`center`}
+                      color={`white`}
+                    />
+                    <TextView
+                      text={app.description}
+                      heading={5}
+                      align={`center`}
+                      color={`white`}
+                    />
                   </Column>
                 </CardView>
               )
@@ -84,16 +120,38 @@ class Marketplace extends React.Component {
         <Row styling='splash-parallax'>
           <Column width='2-3' horizontalAlignment='center'>
             <TextView text={`All Apps`} heading={2} align={`left`} />
-            <TextView text={`Every app made using the API`} heading={5} align={`left`} />
+            <TextView
+              text={`Every app made using the API`}
+              heading={5}
+              align={`left`}
+            />
             {appsToRender.map((app, i) => {
               return (
-                <CardView key={`all-apps-` + i} width={`1-2`} type={`alternate`} link={`/marketplace/` + app.id}
+                <CardView
+                  key={`all-apps-` + i}
+                  width={`1-2`}
+                  type={`alternate`}
+                  link={`/marketplace/` + app.id}
                   style={{ 'padding': `20px 0 ` }}
                 >
                   <Column width='9-10' horizontalAlignment='center'>
-                    <ImageView src={app.logolight} width={iconsize} height={iconsize} />
-                    <TextView text={app.name} heading={2} align={`center`} color={`black`} />
-                    <TextView text={app.description} heading={5} align={`center`} color={`black`} />
+                    <ImageView
+                      src={app.logolight}
+                      width={iconsize}
+                      height={iconsize}
+                    />
+                    <TextView
+                      text={app.name}
+                      heading={2}
+                      align={`center`}
+                      color={`black`}
+                    />
+                    <TextView
+                      text={app.description}
+                      heading={5}
+                      align={`center`}
+                      color={`black`}
+                    />
                   </Column>
                 </CardView>
               )
@@ -140,49 +198,98 @@ class AppPage extends React.Component {
       <>
         <NavBar isScroll={false} />
 
-        <Row height='300px' style={{ margin: `60px 0 0 0` }} styling='splash-parallax'>
-          <Column width='2-3' horizontalAlignment='center' verticalAlignment='center'>
+        <Row
+          height='300px'
+          style={{ margin: `60px 0 0 0` }}
+          styling='splash-parallax'
+        >
+          <Column
+            width='2-3'
+            horizontalAlignment='center'
+            verticalAlignment='center'
+          >
             <TextView text={`UCL Marketplace`} heading={1} align={`center`} />
-            <TextView text={`Apps that use UCL API`} heading={2} align={`center`} />
+            <TextView
+              text={`Apps that use UCL API`}
+              heading={2}
+              align={`center`}
+            />
           </Column>
         </Row>
 
         <Row styling='secondary' height='70px' style={{ "padding": `10px 0` }}>
           <Column width='2-3' horizontalAlignment='center'>
-            <Column width='fit-content' minWidth={iconsize} typeOfInline='grid' horizontalAlignment='left'>
-              <ButtonView type='alternate' text='back' link='/marketplace' 
-                style={{ 'float' : `left`,
-'margin': `10px 0` }}
+            <Column
+              width='fit-content'
+              minWidth={iconsize}
+              typeOfInline='grid'
+              horizontalAlignment='left'
+            >
+              <ButtonView type='alternate'
+                text='back'
+                link='/marketplace'
+                style={{
+                  'float': `left`,
+                  'margin': `10px 0`,
+                }}
               />
             </Column>
           </Column>
         </Row>
 
         <Row styling='secondary' height='100px' noPadding>
-          <Column width='2-3' horizontalAlignment='center' verticalAlignment='center'>
-            <Column width='fit-content' minWidth={iconsize} typeOfInline='grid' horizontalAlignment='left'>
-              <ImageView src={logodark} width={iconsize} height={iconsize}
-                description={name + `logo`} centred style={{ 'margin': `0 auto 0 0` }}
+          <Column
+            width='2-3'
+            horizontalAlignment='center'
+            verticalAlignment='center'
+          >
+            <Column
+              width='fit-content'
+              minWidth={iconsize}
+              typeOfInline='grid'
+              horizontalAlignment='left'
+            >
+              <ImageView
+                src={logodark}
+                width={iconsize}
+                height={iconsize}
+                description={name + `logo`}
+                centred
+                style={{ 'margin': `0 auto 0 0` }}
               />
             </Column>
-            <Column width='fit-content' minWidth={iconsize} horizontalAlignment='left' textAlign='left' style={{ "paddingLeft": `20px` }}>
+            <Column
+              width='fit-content'
+              minWidth={iconsize}
+              horizontalAlignment='left'
+              textAlign='left'
+              style={{ "paddingLeft": `20px` }}
+            >
               <TextView text={name} heading={2} />
               <TextView text={description} heading={5} />
             </Column>
           </Column>
         </Row>
-        <Row styling='secondary' style={ { 'padding' : `0` } }>
+        <Row styling='secondary' style={{ 'padding': `0` }}>
           <Column width='2-3' horizontalAlignment='center'>
             {screenshots.map((img, i) => (
-              <CardView width={`1-`+screenshots.length} minWidth={screenshotwidth} type='no-bg' key={name + ` screenshot number ` + i}>
-                <ImageView src={img} width={screenshotwidth} height={screenshotheight}
-                  description={name + ` screenshot number ` + i} centred
+              <CardView
+                width={`1-` + screenshots.length}
+                minWidth={screenshotwidth}
+                type='no-bg'
+                key={name + ` screenshot number ` + i}
+              >
+                <ImageView src={img}
+                  width={screenshotwidth}
+                  height={screenshotheight}
+                  description={name + ` screenshot number ` + i}
+                  centred
                 />
               </CardView>
             ))}
           </Column>
         </Row>
-        <Row styling='secondary' style={ { 'padding' : `0 0 20px 0` } }>
+        <Row styling='secondary' style={{ 'padding': `0 0 20px 0` }}>
           <Column width='1-2' horizontalAlignment='center' textAlign='left'>
             {detailedDescription}
           </Column>
@@ -191,21 +298,28 @@ class AppPage extends React.Component {
         {links.length > 0 ? (
           <Row styling='splash-parallax'>
             <Column width='2-3' horizontalAlignment='center'>
-              {links.map( (x, key) => (
-                <ButtonView text={x.name} link={x.link} 
-                  type={key%2==0 ? `default` : `alternate`} 
-                  key={key} style={{ "marginLeft": `0` }}
+              {links.map((x, key) => (
+                <ButtonView text={x.name}
+                  link={x.link}
+                  type={key % 2 == 0 ? `default` : `alternate`}
+                  key={key}
+                  style={{ "marginLeft": `0` }}
                 />
               ))}
             </Column>
           </Row>
-        ) : ( 
-          <Row styling='splash-parallax'>
-            <Column width='2-3' horizontalAlignment='center'>
-              <TextView text='No downloads available' heading={3} align={`center`} style={ { "margin" : `0` } }/>
-            </Column>
-          </Row>
-        )}
+        ) : (
+            <Row styling='splash-parallax'>
+              <Column width='2-3' horizontalAlignment='center'>
+                <TextView
+                  text='No downloads available'
+                  heading={3}
+                  align={`center`}
+                  style={{ "margin": `0` }}
+                />
+              </Column>
+            </Row>
+          )}
 
         <Footer />
 
