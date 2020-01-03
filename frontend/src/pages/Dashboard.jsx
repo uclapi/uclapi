8a7642a3e5396b15e035146862bcd253020b3d60
TuxedoFish <harryliversedge@gmail.com>
2020-01-03 23:28:52 +0000

    Started to design card from anush sketch

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -14,6 +14,8 @@ import Layout from '../components/dashboard/layout.jsx'
 class Dashboard extends React.Component {
   constructor(props) {
     super(props)
+
+    // Sort the apps by last updated property
     window.initialData.apps.sort((a, b) => {
       const dateA = dayjs(a.created)
       const dateB = dayjs(b.created)
@@ -36,9 +38,23 @@ class Dashboard extends React.Component {
         <NavBar isScroll={false} />
 
         <Row height='fit-content' styling='splash-parallax' style={{ minHeight : `100%`}}>
-          <Column width='2-3' horizontalAlignment='center' style={{ marginTop: `50px` }}>
+          <Column width='2-3' horizontalAlignment='center' style={{ marginTop: `80px` }}>
             <TextView text={`Welcome, ` + name} heading={1} align={`left`} />
-            <TextView text={`Your username is: ` + cn} heading={2} align={`left`} />
+            <TextView text={`Your username is: ` + cn} heading={2} align={`left`} color={'#ddd'}/>
+
+            {apps.map( (app, index) => (
+              <CardView width='1-1' minWidth='280px' type='default' noPadding>
+                <Row height='200px' styling='transparent' noPadding>
+                  <CardView width='1-2' minWidth="100px" type="no-bg" snapAlign>
+                    <TextView text={app.name} heading={3} align={`left`} />
+                  </CardView>
+                  <CardView width='1-2' minWidth="100px" type="no-bg" snapAlign>
+                    <TextView text={"Created: " + app.created} heading={5} align={`right`} style={{margin: '0'}}/>
+                    <TextView text={"Updated: " + app.updated} heading={5} align={`right`} style={{margin: '0'}}/>
+                  </CardView>
+                </Row>
+              </CardView>
+            ))}
 
             <AppList apps={apps} />
           </Column>
