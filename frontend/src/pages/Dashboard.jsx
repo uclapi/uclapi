645af4959dc976625df6ce387d83a8b4479afa0c
TuxedoFish <harryliversedge@gmail.com>
2020-01-04 16:08:33 +0000

    Added days ago for updated and created dates and also added the token view

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,15 +5,51 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // Components
-import { ButtonView, CardView, Column, Demo, 
-  Footer, ImageView, NavBar, Row, TextView } from 'Layout/Items.jsx'
+import { CardView, Column,
+  Footer, NavBar, Row, TextView } from 'Layout/Items.jsx'
 
-import AppList from '../components/dashboard/appList.jsx'
-import Layout from '../components/dashboard/layout.jsx'
+const styles = {
+  baseText: {
+    color: `white`,
+    fontWeight: `300`,
+  },
+  lightText: {
+    color: `#ddd`,
+    fontWeight: `100`,
+  },
+  dates: {
+    color: `#ddd`,
+    fontWeight: `100`,
+    margin: `0`,
+  },
+  appHolder: {
+    marginTop: `80px`,
+  },
+  tokenHolder: {
+    borderRadius: `0`,
+  },
+  tokenCopyField: {
+    marginTop: `0`,
+    width: `50%`,
+    padding: `15px`,
+    textAlign: `left`,
+  },
+  tokenText: {
+    float: `left`,
+    margin: `6px 10px 0 0`,
+    color: `white`,
+    fontWeight: `300`,
+  },
+}
 
 class Dashboard extends React.Component {
+
   constructor(props) {
     super(props)
+
+    this.DEBUGGING = true
+
+    this.timeSince = this.timeSince.bind(this)
 
     // Sort the apps by last updated property
     window.initialData.apps.sort((a, b) => {
@@ -28,6 +64,7 @@ class Dashboard extends React.Component {
         return 0
       }
     })
+
     this.state = { data: window.initialData }
   }
   render() {
@@ -39,28 +76,89 @@ class Dashboard extends React.Component {
 
         <Row height='fit-content' styling='splash-parallax' style={{ minHeight : `100%`}}>
           <Column width='2-3' horizontalAlignment='center' style={{ marginTop: `80px` }}>
-            <TextView text={`Welcome, ` + name} heading={1} align={`left`} />
-            <TextView text={`Your username is: ` + cn} heading={2} align={`left`} color={'#ddd'}/>
+            <TextView text={`Welcome, ` + name}
+              heading={1}
+              align={`left`} 
+              style={styles.baseText}
+            />
+            <TextView text={`Your username is: ` + cn}
+              heading={2}
+              align={`left`} 
+              style={styles.lightText}
+            />
 
-            {apps.map( (app, index) => (
-              <CardView width='1-1' minWidth='280px' type='default' noPadding>
-                <Row height='200px' styling='transparent' noPadding>
-                  <CardView width='1-2' minWidth="100px" type="no-bg" snapAlign>
-                    <TextView text={app.name} heading={3} align={`left`} />
-                  </CardView>
-                  <CardView width='1-2' minWidth="100px" type="no-bg" snapAlign>
-                    <TextView text={"Created: " + app.created} heading={5} align={`right`} style={{margin: '0'}}/>
-                    <TextView text={"Updated: " + app.updated} heading={5} align={`right`} style={{margin: '0'}}/>
-                  </CardView>
-                </Row>
-              </CardView>
-            ))}
+            <div className="app-holder" style={styles.appHolder}>
+              {apps.map( (app, index) => {
+                const updated = this.timeSince(new Date(app.updated))
+                const created = this.timeSince(new Date(app.updated))
 
-            <AppList apps={apps} />
+                return (
+                  <CardView width='1-1' minWidth='280px' type='default' key={index} noPadding>
+                    <Row styling='transparent' noPadding>
+                      <CardView width='1-2' minWidth="100px" type="no-bg" snapAlign>
+                        <TextView text={app.name}
+                          heading={3}
+                          align={`left`} 
+                          style={styles.baseText}
+                        />
+                      </CardView>
+                      <CardView width='1-2' minWidth="100px" type="no-bg" snapAlign>
+                        <TextView text={`Created: ` + created + ` ago`}
+                          heading={5}
+                          align={`right`} 
+                          style={styles.dates}
+                        />
+                        <TextView text={`Updated: ` + updated + ` ago`}
+                          heading={5}
+                          align={`right`}
+                          style={styles.dates}
+                        />
+                      </CardView>
+                    </Row>
+                    <Row styling='transparent' noPadding>
+                      <CardView width='1-1' type="no-bg" style={styles.tokenHolder}>
+                        <TextView text={`API Token:`} heading={3} align={`left`} style={styles.tokenText} />
+                        <input type="text" className="token-input" readOnly value={app.token} style={styles.tokenCopyField}/>
+                      </CardView>
+                    </Row>
+                  </CardView>
+                  )
+              }
+              )}
+            </div>
           </Column>
         </Row>
+
+        <Footer />
       </>
     )
+  }
+
+  timeSince(date) {
+    const seconds = Math.floor((new Date() - date) / 1000)
+
+    let interval = Math.floor(seconds / 31536000)
+
+    if (interval > 1) {
+      return interval + ` years`
+    }
+    interval = Math.floor(seconds / 2592000)
+    if (interval > 1) {
+      return interval + ` months`
+    }
+    interval = Math.floor(seconds / 86400)
+    if (interval > 1) {
+      return interval + ` days`
+    }
+    interval = Math.floor(seconds / 3600)
+    if (interval > 1) {
+      return interval + ` hours`
+    }
+    interval = Math.floor(seconds / 60)
+    if (interval > 1) {
+      return interval + ` minutes`
+    }
+    return Math.floor(seconds) + ` seconds`
   }
 }
 
