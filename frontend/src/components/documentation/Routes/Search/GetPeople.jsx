8f4a2d69e210c94a9b814e21491f69f60e964a71
Huey <leejinhuey@gmail.com>
2020-01-18 12:05:54 +0000

    fix outdated people docs

--- frontend/src/components/documentation/Routes/Search/GetPeople.jsx
+++ frontend/src/components/documentation/Routes/Search/GetPeople.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import PropTypes from 'prop-types'
+import React from 'react'
 
-import Topic from './../../Topic.jsx';
-import Table from './../../Table.jsx';
-import Cell from './../../Cell.jsx';
+import Cell from './../../Cell.jsx'
+import Table from './../../Table.jsx'
+import Topic from './../../Topic.jsx'
 
 
-let codeExamples = {
+const codeExamples = {
   python: `import requests
 
 params = {
@@ -15,10 +16,12 @@ params = {
 r = requests.get("https://uclapi.com/search/people", params=params)
 print(r.json())`,
 
+  // eslint-disable-next-line no-secrets/no-secrets
   shell: `curl -G https://uclapi.com/search/people \\
 -d token=uclapi-5d58c3c4e6bf9c-c2910ad3b6e054-7ef60f44f1c14f-a05147bfd17fdb \\
 -d query='Jane'`,
 
+  // eslint-disable-next-line no-secrets/no-secrets
   javascript: `fetch("https://uclapi.com/search/people?token=uclapi-5d58c3c4e6bf9c-c2910ad3b6e054-7ef60f44f1c14f-a05147bfd17fdb&query=Jane")
 .then((response) => {
   return response.json()
@@ -26,11 +29,11 @@ print(r.json())`,
 .then((json) => {
   console.log(json);
 })
-`
+`,
 }
 
 
-let response = `{
+const response = `{
   "ok": true,
   "people": [
     {
@@ -44,95 +47,124 @@ let response = `{
 }
 `
 
-let responseCodeExample = {
+const responseCodeExample = {
   python: response,
   javascript: response,
-  shell: response
+  shell: response,
 }
 
 
 export default class GetEquiment extends React.Component {
 
-    render () {
-      return (
-        <div>
-          <Topic
-            activeLanguage={this.props.activeLanguage}
-            codeExamples={codeExamples}>
-            <h1 id="search/people">Get People</h1>
-            <p>
-              This endpoint returns matching people and information about them.
+  static propTypes = {
+    activeLanguage: PropTypes.string,
+  }
+
+  render() {
+    const { activeLanguage } = this.props
+    return (
+      <div>
+        <Topic
+          activeLanguage={activeLanguage}
+          codeExamples={codeExamples}
+        >
+          <h1 id="search/people">Get People</h1>
+          <p>
+            This endpoint returns matching people and information about them.
             </p>
-            <p>
-              <i>
-                Note: This endpoint only returns a maximum of 20 matches.
+          <p>
+            <i>
+              Note: This endpoint only returns a maximum of 20 matches.
               </i>
-            </p>
+          </p>
+          <p>
+            <i>
+              Following a change to UCL&apos;s systems in 2019, this endpoint only returns
+              staff. Students will not be returned through this API.
+            </i>
+          </p>
 
-            <Table
-              name="Query Parameters">
-              <Cell
-                name="token"
-                requirement="required"
-                example="uclapi-5d58c3c4e6bf9c-c2910ad3b6e054-7ef60f44f1c14f-a05147bfd17fdb"
-                description="Authentication token." />
-              <Cell
-                name="query"
-                requirement="required"
-                example="Jane"
-                description="Name of the person you are searching for." />
-            </Table>
-          </Topic>
+          <Table
+            name="Query Parameters"
+          >
+            <Cell
+              name="token"
+              requirement="required"
+              example="uclapi-5d58c3c4e6bf9c-c2910ad3b6e054-7ef60f44f1c14f-a05147bfd17fdb"
+              description="Authentication token."
+            />
+            <Cell
+              name="query"
+              requirement="required"
+              example="Jane"
+              description="Name of the person you are searching for."
+            />
+          </Table>
+        </Topic>
 
-          <Topic
-            activeLanguage={this.props.activeLanguage}
-            codeExamples={responseCodeExample}>
-            <h2>Response</h2>
-            <p>
+        <Topic
+          activeLanguage={this.props.activeLanguage}
+          codeExamples={responseCodeExample}
+        >
+          <h2>Response</h2>
+          <p>
             The people field contains a list of people that match your query.
             </p>
-            <Table
-              name="Response">
-              <Cell
-                name="name"
-                extra="string"
-                example="Jane Doe"
-                description="The name of the person."/>
-              <Cell
-                name="status"
-                extra="string"
-                example="Staff"
-                description="Tells us whether the person is a student or member of staff."/>
-              <Cell
-                name="department"
-                extra="string"
-                example="UCL Medical School"
-                description="The department the student studies or works under."/>
-              <Cell
-                name="email"
-                extra="string"
-                example="jane.doe.17@ucl.ac.uk"
-                description="The email of the person."/>
-            </Table>
-          </Topic>
+          <Table
+            name="Response"
+          >
+            <Cell
+              name="name"
+              extra="string"
+              example="Jane Doe"
+              description="The name of the person."
+            />
+            <Cell
+              name="status"
+              extra="string"
+              example="Staff"
+              description={`
+                Since only staff are returned through this API,
+                this should always be Staff.`
+              }
+            />
+            <Cell
+              name="department"
+              extra="string"
+              example="UCL Medical School"
+              description="The department the student studies or works under."
+            />
+            <Cell
+              name="email"
+              extra="string"
+              example="jane.doe.17@ucl.ac.uk"
+              description="The email of the person."
+            />
+          </Table>
+        </Topic>
 
-          <Topic
-            noExamples={true}>
-            <Table
-              name="Errors">
-              <Cell
-                name="No token provided"
-                description="Gets returned when you have not supplied a token in your request." />
-              <Cell
-                name="Token does not exist"
-                description="Gets returned when you supply an invalid token." />
-              <Cell
-                name="No query provided"
-                description="Gets returned when you have not supplied a query in your request." />
-            </Table>
-          </Topic>
-        </div>
-      )
-    }
+        <Topic
+          noExamples
+        >
+          <Table
+            name="Errors"
+          >
+            <Cell
+              name="No token provided"
+              description="Gets returned when you have not supplied a token in your request."
+            />
+            <Cell
+              name="Token does not exist"
+              description="Gets returned when you supply an invalid token."
+            />
+            <Cell
+              name="No query provided"
+              description="Gets returned when you have not supplied a query in your request."
+            />
+          </Table>
+        </Topic>
+      </div>
+    )
+  }
 
 }
