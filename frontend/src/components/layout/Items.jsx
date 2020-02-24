efc76326882181bbfc7288691a5516b77fc37923
Huey <leejinhuey@gmail.com>
2020-02-24 10:14:57 +0000

    removed unnecessary LanguageTabs and fixed #2004

--- frontend/src/components/layout/Items.jsx
+++ frontend/src/components/layout/Items.jsx
@@ -1,29 +1,28 @@
 // Containers
-import Row from './containers/Row.jsx';
-import Column from './containers/Column.jsx';
-
-// Views
-import TextView from './views/TextView.jsx';
-import ButtonView from './views/ButtonView.jsx';
-import CardView from './views/CardView.jsx';
-import ImageView from './views/ImageView.jsx';
-import CodeView from './views/CodeView.jsx';
-import AutoCompleteView from './views/AutoCompleteView.jsx';
+import Column from './containers/Column.jsx'
+import Row from './containers/Row.jsx'
 // Custom UCL API Components
-import Demo from './Demo.jsx';
-import NavBar from './NavBar.jsx';
-import Footer from './Footer.jsx';
+import Demo from './Demo.jsx'
+import Footer from './Footer.jsx'
+import NavBar from './NavBar.jsx'
+import AutoCompleteView from './views/AutoCompleteView.jsx'
+import ButtonView from './views/ButtonView.jsx'
+import CardView from './views/CardView.jsx'
+import CodeView from './views/CodeView.jsx'
+import ImageView from './views/ImageView.jsx'
+// Views
+import TextView from './views/TextView.jsx'
 
 export {
-    Row,
-    Column,
-    TextView, 
-    ButtonView, 
-    CardView,
-    ImageView, 
-    CodeView,
-    Demo, 
-    NavBar, 
-    Footer, 
-    AutoCompleteView
+  Row,
+  Column,
+  TextView,
+  ButtonView,
+  CardView,
+  ImageView,
+  CodeView,
+  Demo,
+  NavBar,
+  Footer,
+  AutoCompleteView,
 }
