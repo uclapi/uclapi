d289c4d3acc7d14bff60ce0079113c939c1a2973
TuxedoFish <harryliversedge@gmail.com>
2020-03-05 15:26:40 +0000

    Fixed social media button styling

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -19,19 +19,25 @@ export default class Footer extends React.Component {
 
     return (
       <Row styling='secondary' style={{ padding: `20px 0` }}>
-        <Column width='1-1' horizontalAlignment='center' className="column-horizontal">
-          <CardView width='1-3' type='github' link='https://github.com/uclapi'>
-            <ImageView src={github} width={logosize} height={logosize} description={`github logo`} isCentered />
-          </CardView>
+        <div className="social-media-holder">
+          <a href="https://github.com/uclapi">
+            <div className="uclapi-card-github" style={{ width: logosize, height: logosize}} >
+              <ImageView src={github} width={logosize} height={logosize} description={`github logo`} isCentered />
+            </div>
+          </a>
 
-          <CardView width='1-3' type='facebook' link='https://www.facebook.com/uclapi/'>
-            <ImageView src={facebook} width={logosize} height={logosize} description={`facebook logo`} isCentered />
-          </CardView>
+          <a href="https://www.facebook.com/uclapi/">
+            <div className="uclapi-card-facebook" style={{ width: logosize, height: logosize}} >
+              <ImageView src={facebook} width={logosize} height={logosize} description={`facebook logo`} isCentered />
+            </div>
+          </a>
 
-          <CardView width='1-3' type='twitter' link='https://twitter.com/uclapi'>
-            <ImageView src={twitter} width={logosize} height={logosize} description={`twitter logo`} isCentered />
-          </CardView>
-        </Column>
+          <a href="https://twitter.com/uclapi">
+            <div className="uclapi-card-twitter" style={{ width: logosize, height: logosize}} >
+              <ImageView src={twitter} width={logosize} height={logosize} description={`twitter logo`} isCentered />
+            </div>
+          </a>
+        </div>
       </Row >
     )
   }
