4b117bb987d2960c1c66db81153a8cc5b024892a
zipy124 <zak.morgan.17@ucl.ac.uk>
2020-06-30 18:50:07 +0100

    Add most popular method and service endpoints, as-well as a quota endpoint

--- backend/uclapi/dashboard/urls.py
+++ backend/uclapi/dashboard/urls.py
@@ -2,14 +2,18 @@ from django.conf.urls import url
 
 from dashboard.api_applications import (
     create_app, delete_app, regenerate_app_token, rename_app, set_callback_url,
-    update_scopes, number_of_requests
+    update_scopes, number_of_requests, quota_remaining, most_popular_service,
+    most_popular_method
 )
 
 from . import views, webhook_views
 
 urlpatterns = [
     url(r'^$', views.dashboard),
-    url(r'api/analytics/requests/$', number_of_requests),
+    url(r'api/analytics/requests/total$', number_of_requests),
+    url(r'api/analytics/requests/quota$', quota_remaining),
+    url(r'api/analytics/requests/services$', most_popular_service),
+    url(r'api/analytics/requests/methods$', most_popular_method),
     url(r'api/create/$', create_app),
     url(r'api/rename/$', rename_app),
     url(r'api/regen/$', regenerate_app_token),
