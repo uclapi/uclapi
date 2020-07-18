d8f7d86e684cb9d90d3980a8bddd2b486e632983
Zak Morgan <iiianmmm@gmail.com>
2020-07-18 17:41:00 +0100

    Add date filter to documentation (#2446)
    * Add date filter to documentation
    
    * Fix spacing to satisfy hound
    
    * Fix spacing to satisfy hound
    
    * Change argument to 'date' as-well as re-ordering documentation to have mandatory arguments before optional
    
    * Satisfy hound by reducing indent to 2 spaces instead of 4
    
    * Update frontend/src/components/documentation/Routes/Timetable/GetPersonalTimetable.jsx
    
    Co-authored-by: Feverfew <Feverfew@users.noreply.github.com>
    
    * Fix ordering or parameters in doc and fix paramter name.
    
    * fix indentation
    
    Co-authored-by: Feverfew <Feverfew@users.noreply.github.com>

--- backend/uclapi/timetable/views.py
+++ backend/uclapi/timetable/views.py
@@ -34,11 +34,8 @@ def get_personal_timetable_endpoint(request, *args, **kwargs):
     """
     token = kwargs['token']
     user = token.user
-    try:
-        date_filter = request.GET["date_filter"]
-        timetable = get_student_timetable(user.employee_id, date_filter)
-    except KeyError:
-        timetable = get_student_timetable(user.employee_id)
+    date_filter = request.GET.get("date")
+    timetable = get_student_timetable(user.employee_id, date_filter)
 
     response = {
         "ok": True,
@@ -66,7 +63,7 @@ def get_modules_timetable_endpoint(request, *args, **kwargs):
 
     modules = module_ids.split(',')
 
-    date_filter = request.GET.get("date_filter")
+    date_filter = request.GET.get("date")
     custom_timetable = get_custom_timetable(modules, date_filter)
 
     if custom_timetable:
