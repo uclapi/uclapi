541238194546d1a8db2a181c64868bba22648570
Huey <hello@huey.xyz>
2020-09-17 10:24:42 +0800

    ✂  decouple warning & error pages from django

--- frontend/src/pages/Warning.jsx
+++ frontend/src/pages/Warning.jsx
@@ -1,74 +1,15 @@
-import warning from 'Images/warning/warning.svg'
-import {
-  Button,
-  Column,
-  Container,
-  Footer,
-  ImageView,
-  NavBar,
-  Row,
-  TextView,
-} from 'Layout/Items.jsx'
+import Warning from 'Layout/Warning.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'Styles/common/uclapi.scss'
-import 'Styles/navbar.scss'
 
-const Warning = () => {
-  const title = window.initialData.title
-  const content = window.initialData.content
-
-  return (
-    <>
-      <NavBar isScroll={false} />
-
-      {/* Warning message */}
-
-      <Container styling="splash-parallax" height="600px">
-        <Row 
-          width="2-3"
-          horizontalAlignment="center"
-          verticalAlignment="center"
-        >
-          
-          <Column width="1-2" className="default">
-            <ImageView
-              src={warning}
-              width="367px"
-              height="405px"
-              description="large exclamation point, warning!"
-            />
-          </Column>
-
-          <Column width="1-2" alignItems="column">
-            <TextView 
-              heading="1"
-              text={title}
-              align="left"
-            />
-            {content.map((line, key) =>
-              <TextView 
-                heading="3"
-                text={line}
-                align="left"
-                key={key}
-              />
-            )}
-            <Button
-              link={`/`} 
-              centred 
-            >
-              Home
-            </Button>
-          </Column>
-        </Row>
-      </Container>
-      <Footer />
-    </>
-  )
-}
+const WarningPage = () => (
+  <Warning
+    title="Please note you are not fully logged out!"
+    content="You have been logged out from UCL API. However, in order to be fully logged out of all UCL services, you need to close your browser completely and re-open it. Thank you! Click here to go back to the front page:"
+  />
+)
 
 ReactDOM.render(
-  <Warning />,
+  <WarningPage />,
   document.querySelector(`.app`)
 )
