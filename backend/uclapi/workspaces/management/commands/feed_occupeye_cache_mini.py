c1b0ac93e736e22091259f502b7f6010af5877c4
Greenfrogs <5961364+greenfrogs@users.noreply.github.com>
2020-12-03 18:54:07 +0000

    :pencil2: Revert workspaces management refactor

--- backend/uclapi/workspaces/management/commands/feed_occupeye_cache_mini.py
+++ backend/uclapi/workspaces/management/commands/feed_occupeye_cache_mini.py
@@ -2,9 +2,7 @@ from django.core.management.base import BaseCommand
 
 from common.cachet import (
     get_incident_name,
-    CachetException,
-    create_incident,
-    update_incident,
+    CachetException, create_incident, update_incident
 )
 from workspaces.occupeye.cache import OccupeyeCache
 
@@ -12,7 +10,9 @@ from cachetclient.v1 import enums
 
 
 class Command(BaseCommand):
-    help = "Caches current OccupEye sensor statuses into Redis"
+    help = (
+        'Caches current OccupEye sensor statuses into Redis'
+    )
 
     def handle(self, *args, **options):
         try:
@@ -24,30 +24,30 @@ class Command(BaseCommand):
             incident_name = get_incident_name("Occupeye-Mini")
             if incident_name:
                 try:
-                    update_incident(
-                        "Occupeye Mini-Cache succeeded",
-                        incident_name,
-                        enums.INCIDENT_FIXED,
-                    )
+                    update_incident("Occupeye Mini-Cache succeeded",
+                                    incident_name, enums.INCIDENT_FIXED)
                 except CachetException as cachet_error:
-                    print(f"Failed to create fixed cachet incident. " f"Reason: {repr(cachet_error)}")
+                    print(f"Failed to create fixed cachet incident. "
+                          f"Reason: {repr(cachet_error)}")
                 except Exception as cachet_error:
-                    print(f"Unexpected: Failed to create fixed cachet " f"incident. " f"Reason: {repr(cachet_error)}")
+                    print(f"Unexpected: Failed to create fixed cachet "
+                          f"incident. "
+                          f"Reason: {repr(cachet_error)}")
             else:
                 print("Could not find appropriate incident in Cachet!")
         except Exception as occupeye_error:
             incident_name = get_incident_name("Occupeye-Mini")
             if incident_name:
                 try:
-                    create_incident(
-                        occupeye_error,
-                        incident_name,
-                        enums.INCIDENT_INVESTIGATING,
-                        enums.COMPONENT_STATUS_MAJOR_OUTAGE,
-                    )
+                    create_incident(occupeye_error,
+                                    incident_name,
+                                    enums.INCIDENT_INVESTIGATING,
+                                    enums.COMPONENT_STATUS_MAJOR_OUTAGE)
                 except CachetException as cachet_error:
-                    print(f"Failed to create cachet incident. " f"Reason: {repr(cachet_error)}")
+                    print(f"Failed to create cachet incident. "
+                          f"Reason: {repr(cachet_error)}")
                 except Exception as cachet_error:
-                    print(f"Unexpected: Failed to create cachet incident. " f"Reason: {repr(cachet_error)}")
+                    print(f"Unexpected: Failed to create cachet incident. "
+                          f"Reason: {repr(cachet_error)}")
             else:
                 print("Could not find appropriate incident in Cachet!")
