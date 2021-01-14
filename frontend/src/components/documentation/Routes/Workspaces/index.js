31b95c9aa4cd2d6b7ec276805b5c6b23aa286fba
Shubham Jain <shubham@sjain.dev>
2021-01-14 19:10:08 +0000

    Remove manually-written documentation

--- frontend/src/components/documentation/Routes/Workspaces/index.js
+++
@@ -1,8 +0,0 @@
-export { default as GetImage } from './GetImage.jsx'
-export { default as GetLastSensorUpdate } from './GetLastSensorUpdate.jsx'
-export { default as GetLiveImage } from './GetLiveImage.jsx'
-export { default as GetSensorHistoricalTimeData} from './GetSensorHistoricalTimeData.jsx'
-export { default as GetSensors } from './GetSensors.jsx'
-export { default as GetSensorSummary } from './GetSensorSummary.jsx'
-export { default as GetSurveys } from './GetSurveys.jsx'
-
--- frontend/src/components/documentation/Routes/WorkspacesHistorical/index.js
+++
@@ -1,3 +0,0 @@
-export { default as WorkspacesHistoricalList } from './HistoricalList.jsx'
-export { default as WorkspacesHistoricalListSurveys } from './ListSurveys.jsx'
-export { default as WorkspacesHistoricalListSensors } from './ListSensors.jsx'
