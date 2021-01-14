6f467cea4bd5c8ea50f93323b6adbd757dec4116
Shubham Jain <shubham@sjain.dev>
2021-01-14 19:32:14 +0000

    Fix new documentation styling
    Also removes unused styling.

---
+++ frontend/src/pages/Documentation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'Styles/documentation.scss'
+import { NavBar } from "../components/layout/Items.jsx"
+import SwaggerUI from "swagger-ui-react"
+import "Styles/common/uclapi.scss"
+import "Styles/navbar.scss"
+import "swagger-ui-react/swagger-ui.css"
+
+const Documentation = () => (
+  <div className='vertical-padding'>
+    <NavBar isScroll={false} />
+    <SwaggerUI url="https://cdn.jsdelivr.net/gh/uclapi/uclapi-openapi/uclapi.json" />
+  </div>
+)
+
+ReactDOM.render(
+  <Documentation />,
+  document.querySelector(`.app`)
+)
