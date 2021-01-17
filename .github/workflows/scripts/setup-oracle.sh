6085b708e56cecd8f09865b7f656def99e15bb39
Huey <hello@huey.xyz>
2021-01-17 23:33:44 +0800

    cd in first

--- .github/workflows/scripts/setup-oracle.sh
+++ .github/workflows/scripts/setup-oracle.sh
@@ -43,37 +43,4 @@ sudo ln -s libclntshcore.so.$ORACLE_SO_VERSION libclntshcore.so
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
 sudo ldconfig
 
-# Add ORACLE_HOME to /etc/environment
-# grep -q -F "ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION" /etc/environment || sudo bash -c 'echo "ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION"' >> /etc/environment
-
-# # Add ld path to /etc/ld.so.conf.d
-# sudo echo "/opt/oracle/instantclient_$ORACLE_VERSION" > /etc/ld.so.conf.d/oracle.conf
-# sudo ldconfig
-
 popd
-
-# ORACLE_INSTANTCLIENT_BASIC_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-basic-linux.x64-12.2.0.1.0.zip
-# ORACLE_INSTANTCLIENT_SDK_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-sdk-linux.x64-12.2.0.1.0.zip
-
-# apt-get update && \
-#   apt-get install -y libaio1 unzip wget build-essential libpq-dev libpq5 && \
-# wget https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-basic-linux.x64-12.2.0.1.0.zip && \
-#   wget https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-sdk-linux.x64-12.2.0.1.0.zip && \
-#   unzip instantclient-basic-linux.x64-12.2.0.1.0.zip && \
-#   unzip instantclient-sdk-linux.x64-12.2.0.1.0.zip && \
-#   cd instantclient_12_2 && \
-#   ln -s libclntsh.so.12.1 libclntsh.so && \
-#   ln -s libocci.so.12.1 libocci.so && \
-#   export ORACLE_HOME=$(pwd) && \
-#   export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$ORACLE_HOME && \
-#   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
-#   cd .. && \
-# cd ./backend/uclapi && \
-#   #(sed '/cx.*/d' requirements.txt | sed -e 's/^\\-e //') | xargs -n 1 pip install && \
-#   pip install -r requirements.txt && \
-#   pip install codecov && \
-#   pip install $(cat requirements.txt | grep "cx-Oracle") && \
-#   cd ../.. && \
-# cp ./backend/uclapi/webpack-stats.sample.json ./backend/uclapi/static/webpack-stats.json && \
-#   cd ./frontend/ && \
-#   npm ci
