8b40d41112207847ac471df08efdf650d336a528
Huey <hello@huey.xyz>
2021-01-17 12:16:13 +0800

    there is only 1 python

--- .github/workflows/scripts/setup-dependencies.sh
+++ .github/workflows/scripts/setup-dependencies.sh
@@ -20,9 +20,9 @@ wget https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-basic-linux.
   export LD_LIBRARY_PATH=LD_LIBRARY_PATH:$ORACLE_HOME && \
   cd .. && \
 cd ./backend/uclapi && \
-  (sed '/cx.*/d' requirements.txt | sed -e 's/^\\-e //') | xargs -n 1 pip3 install && \
-  pip3 install codecov && \
-  pip3 install $(cat requirements.txt | grep "cx-Oracle") && \
+  (sed '/cx.*/d' requirements.txt | sed -e 's/^\\-e //') | xargs -n 1 pip install && \
+  pip install codecov && \
+  pip install $(cat requirements.txt | grep "cx-Oracle") && \
   cd ../.. && \
 cp ./backend/uclapi/webpack-stats.sample.json ./backend/uclapi/static/webpack-stats.json && \
   cd ./frontend/ && \
