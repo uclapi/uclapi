1cf965933fd440d092ffd32f818f3660fe2ee0af
Huey <hello@huey.xyz>
2021-01-17 22:21:34 +0800

    install essential dependencies before running pip

--- .github/workflows/scripts/setup-oracle.sh
+++ .github/workflows/scripts/setup-oracle.sh
@@ -22,33 +22,33 @@ ORACLE_SO_VERSION=12.1
 ORACLE_INSTANTCLIENT_BASIC_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-basic-linux.x64-12.2.0.1.0.zip
 ORACLE_INSTANTCLIENT_SDK_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-sdk-linux.x64-12.2.0.1.0.zip
 
-mkdir /opt/oracle
+# mkdir /opt/oracle
 
-wget -O instantclient.zip "$ORACLE_INSTANTCLIENT_BASIC_URL"
-wget -O instantclientsdk.zip "$ORACLE_INSTANTCLIENT_SDK_URL"
+# wget -O instantclient.zip "$ORACLE_INSTANTCLIENT_BASIC_URL"
+# wget -O instantclientsdk.zip "$ORACLE_INSTANTCLIENT_SDK_URL"
 
-unzip -d/opt/oracle instantclient.zip
-unzip -d/opt/oracle instantclientsdk.zip
+# unzip -d/opt/oracle instantclient.zip
+# unzip -d/opt/oracle instantclientsdk.zip
 
-export ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION
+# export ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION
 
-pushd $ORACLE_HOME
+# pushd $ORACLE_HOME
 
-ln -s libclntsh.so.$ORACLE_SO_VERSION libclntsh.so
-ln -s libocci.so.$ORACLE_SO_VERSION libocci.so
-ln -s libclntshcore.so.$ORACLE_SO_VERSION libclntshcore.so
+# ln -s libclntsh.so.$ORACLE_SO_VERSION libclntsh.so
+# ln -s libocci.so.$ORACLE_SO_VERSION libocci.so
+# ln -s libclntshcore.so.$ORACLE_SO_VERSION libclntshcore.so
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
-ldconfig
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
+# ldconfig
 
-# Add ORACLE_HOME to /etc/environment
-grep -q -F "ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION" /etc/environment || echo "ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION" >> /etc/environment
+# # Add ORACLE_HOME to /etc/environment
+# grep -q -F "ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION" /etc/environment || echo "ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION" >> /etc/environment
 
-# Add ld path to /etc/ld.so.conf.d
-echo "/opt/oracle/instantclient_$ORACLE_VERSION" > /etc/ld.so.conf.d/oracle.conf
-ldconfig
+# # Add ld path to /etc/ld.so.conf.d
+# echo "/opt/oracle/instantclient_$ORACLE_VERSION" > /etc/ld.so.conf.d/oracle.conf
+# ldconfig
 
-popd
+# popd
 
 # ORACLE_INSTANTCLIENT_BASIC_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-basic-linux.x64-12.2.0.1.0.zip
 # ORACLE_INSTANTCLIENT_SDK_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-sdk-linux.x64-12.2.0.1.0.zip
