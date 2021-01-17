a389172ae4a2414d7048aecdeb558a0128dd198b
Huey <hello@huey.xyz>
2021-01-17 22:03:34 +0800

    follow our own README

--- .github/workflows/scripts/setup-oracle.sh
+++ .github/workflows/scripts/setup-oracle.sh
@@ -6,18 +6,52 @@
 #   - running on Ubuntu
 #   - is root
 
-apt-get update && \
-  apt-get install -y libaio1 unzip wget build-essential libpq-dev libpq5 && \
-wget https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-basic-linux.x64-12.2.0.1.0.zip && \
-  wget https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-sdk-linux.x64-12.2.0.1.0.zip && \
-  unzip instantclient-basic-linux.x64-12.2.0.1.0.zip && \
-  unzip instantclient-sdk-linux.x64-12.2.0.1.0.zip && \
-  cd instantclient_12_2 && \
-  ln -s libclntsh.so.12.1 libclntsh.so && \
-  ln -s libocci.so.12.1 libocci.so && \
-  export ORACLE_HOME=$(pwd) && \
-  export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$ORACLE_HOME && \
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
+ORACLE_VERSION=12_2
+ORACLE_SO_VERSION=12.1
+
+mkdir /opt/oracle
+
+wget -O instantclient.zip "$ORACLE_INSTANTCLIENT_BASIC_URL"
+wget -O instantclientsdk.zip "$ORACLE_INSTANTCLIENT_SDK_URL"
+
+sudo unzip -d/opt/oracle instantclient.zip
+sudo unzip -d/opt/oracle instantclientsdk.zip
+
+export ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION
+
+pushd $ORACLE_HOME
+
+ln -s libclntsh.so.$ORACLE_SO_VERSION libclntsh.so
+ln -s libocci.so.$ORACLE_SO_VERSION libocci.so
+ln -s libclntshcore.so.$ORACLE_SO_VERSION libclntshcore.so
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
+ldconfig
+
+# Add ORACLE_HOME to /etc/environment
+grep -q -F "ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION" /etc/environment || echo "ORACLE_HOME=/opt/oracle/instantclient_$ORACLE_VERSION" >> /etc/environment
+
+# Add ld path to /etc/ld.so.conf.d
+echo "/opt/oracle/instantclient_$ORACLE_VERSION" > /etc/ld.so.conf.d/oracle.conf
+ldconfig
+
+popd
+
+# ORACLE_INSTANTCLIENT_BASIC_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-basic-linux.x64-12.2.0.1.0.zip
+# ORACLE_INSTANTCLIENT_SDK_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-sdk-linux.x64-12.2.0.1.0.zip
+
+# apt-get update && \
+#   apt-get install -y libaio1 unzip wget build-essential libpq-dev libpq5 && \
+# wget https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-basic-linux.x64-12.2.0.1.0.zip && \
+#   wget https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-sdk-linux.x64-12.2.0.1.0.zip && \
+#   unzip instantclient-basic-linux.x64-12.2.0.1.0.zip && \
+#   unzip instantclient-sdk-linux.x64-12.2.0.1.0.zip && \
+#   cd instantclient_12_2 && \
+#   ln -s libclntsh.so.12.1 libclntsh.so && \
+#   ln -s libocci.so.12.1 libocci.so && \
+#   export ORACLE_HOME=$(pwd) && \
+#   export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$ORACLE_HOME && \
+#   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
 #   cd .. && \
 # cd ./backend/uclapi && \
 #   #(sed '/cx.*/d' requirements.txt | sed -e 's/^\\-e //') | xargs -n 1 pip install && \
