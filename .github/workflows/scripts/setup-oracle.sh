e94211855a3e2502af95b71081f5d22f6c7494d2
Huey <hello@huey.xyz>
2021-01-17 22:29:43 +0800

    build frontend separately

--- .github/workflows/scripts/setup-oracle.sh
+++ .github/workflows/scripts/setup-oracle.sh
@@ -15,14 +15,16 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 apt-get update
-apt-get install -y libaio1 unzip wget build-essential libpq-dev libpq5
+apt-get install -y libaio1 unzip wget build-essential libpq-dev libpq5 sed git locales liblz4-1
 
-ORACLE_VERSION=12_2
-ORACLE_SO_VERSION=12.1
-ORACLE_INSTANTCLIENT_BASIC_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-basic-linux.x64-12.2.0.1.0.zip
-ORACLE_INSTANTCLIENT_SDK_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-sdk-linux.x64-12.2.0.1.0.zip
+export ORACLE_VERSION=12_2
+export ORACLE_SO_VERSION=12.1
+export ORACLE_INSTANTCLIENT_BASIC_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-basic-linux.x64-12.2.0.1.0.zip
+export ORACLE_INSTANTCLIENT_SDK_URL=https://s3.eu-west-2.amazonaws.com/uclapi-static/instantclient-sdk-linux.x64-12.2.0.1.0.zip
+export ORACLE_INSTALL_DIR /opt/oracle
+export ORACLE_HOME ${ORACLE_INSTALL_DIR}/instantclient_${ORACLE_VERSION}
 
-# mkdir /opt/oracle
+mkdir -p /opt/oracle
 
 # wget -O instantclient.zip "$ORACLE_INSTANTCLIENT_BASIC_URL"
 # wget -O instantclientsdk.zip "$ORACLE_INSTANTCLIENT_SDK_URL"
