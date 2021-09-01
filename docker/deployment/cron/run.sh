91c9ab7c9bc00201fdc2253f8a2979e8df5237b5
greenfrogs <5961364+greenfrogs@users.noreply.github.com>
2021-09-01 21:52:34 +0200

    deployment docker: add cron container

---
+++ docker/deployment/cron/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while /bin/true; do
+    # Ensure Supervisor is alive first
+    ps aux | grep supervisor | grep -q -v grep
+    SUPERVISOR_STATUS=$?
+    if [ $SUPERVISOR_STATUS -ne 0 ]; then
+        service supervisor start
+    fi
+
+    # Now check each other service
+    for SERVICE in celery-beat-uclapi celery-uclapi-gencache celery-uclapi-worker
+    do
+        supervisorctl status $SERVICE | grep -q RUNNING
+        if [ $? -ne 0 ]; then
+            echo "$SERVICE"
+            supervisorctl restart $SERVICE
+        fi
+    done
+
+    sleep 60
+done
