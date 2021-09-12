f6f3dae4d5359403e3ccc557493b8283b6001ba7
daohe <daohengliu@gmail.com>
2021-09-12 17:44:05 +0100

    docker cron: run actual cache table task

--- backend/uclapi/timetable/tasks.py
+++ backend/uclapi/timetable/tasks.py
@@ -7,6 +7,9 @@ from datetime import datetime
 import redis
 from celery import shared_task, chord
 from django.conf import settings
+from django.db import connections
+from django import db
+import gc
 
 from common.helpers import LOCAL_TIMEZONE
 from timetable.models import (
@@ -76,9 +79,154 @@ tables = [
 ]
 
 
+# noinspection SqlDialectInspection,PyProtectedMember,PyUnboundLocalVariable
+@shared_task(queue="gencache")
+def cache_table_task(idx, dest_table_idx, load_batch_size=10000, insert_batch_size=5000):
+    table_data = tables[idx]
+
+    gencache_cursor = connections['gencache'].cursor()
+    table_prefix = "roombookings_" if table_data[4] else "timetable_"
+    gencache_cursor.execute("TRUNCATE TABLE {}{} RESTART IDENTITY;".format(
+        table_prefix,
+        table_data[dest_table_idx].__name__.lower()
+    ))
+
+    # Decide whether to use a chunked query or not
+    if table_data[5]:
+        oracle_cursor = connections['roombookings'].cursor()
+        if table_data[3]:
+            if idx == 0:
+                query = "SELECT COUNT(SETID) FROM (SELECT DISTINCT * FROM {} WHERE SETID = '{}')".format(
+                    table_data[0]._meta.db_table,
+                    settings.ROOMBOOKINGS_SETID
+                )
+            else:
+                query = "SELECT COUNT(SETID) FROM {} WHERE SETID = '{}'".format(
+                    table_data[0]._meta.db_table,
+                    settings.ROOMBOOKINGS_SETID
+                )
+
+        else:
+            query = "SELECT COUNT(*) FROM {}".format(
+                table_data[0]._meta.db_table
+            )
+
+        oracle_cursor.execute(query)
+        count_data = oracle_cursor.fetchone()
+        total_records = count_data[0]
+
+        if table_data[3]:
+            if idx == 0:
+                query = "SELECT DISTINCT * FROM {} WHERE SETID = '{}'".format(
+                    table_data[0]._meta.db_table,
+                    settings.ROOMBOOKINGS_SETID
+                )
+            else:
+                query = "SELECT * FROM {} WHERE SETID = '{}'".format(
+                    table_data[0]._meta.db_table,
+                    settings.ROOMBOOKINGS_SETID
+                )
+        else:
+            query = "SELECT * FROM {}".format(
+                table_data[0]._meta.db_table
+            )
+
+        oracle_cursor.arraysize = load_batch_size
+        oracle_cursor.execute(query)
+
+        def columns(cursor):
+            return {cd[0]: i for i, cd in enumerate(cursor.description)}
+
+        cols = columns(oracle_cursor)
+
+        print("[Start] Chunk caching from {} [{} records]".format(
+            table_data[0].__name__,
+            total_records
+        ))
+
+        new_objs = []
+        objs = oracle_cursor.fetchmany(load_batch_size)
+        while objs:
+            for obj in objs:
+                new_obj = {}
+                for k, v in cols.items():
+                    if obj[v]:
+                        val = obj[v]
+                    else:
+                        if isinstance(obj[v], str):
+                            val = ''
+                        elif isinstance(obj[v], int):
+                            val = 0
+                        else:
+                            val = None
+                    new_obj[k.lower()] = val
+
+                new_objs.append(table_data[dest_table_idx](**new_obj))
+
+                if len(new_objs) >= load_batch_size:
+                    table_data[dest_table_idx].objects.using(
+                        'gencache'
+                    ).bulk_create(new_objs, batch_size=insert_batch_size)
+                    new_objs.clear()
+                    gc.collect()
+
+            objs = oracle_cursor.fetchmany(load_batch_size)
+
+        # Insert any items left over
+        table_data[dest_table_idx].objects.using(
+            'gencache'
+        ).bulk_create(new_objs, batch_size=insert_batch_size)
+        del new_objs
+        oracle_cursor.close()
+
+        print("[Done!] Chunk caching from {} [{} records]".format(
+            table_data[0].__name__,
+            total_records
+        ))
+
+    else:
+        if table_data[3]:
+            objs = table_data[0].objects.filter(
+                setid=settings.ROOMBOOKINGS_SETID
+            )
+        else:
+            objs = table_data[0].objects.all()
+
+        new_objs = []
+        item_count = objs.count()
+
+        print("[Start] Loading {} into RAM [{} records]".format(
+            table_data[0].__name__,
+            item_count
+        ))
+
+        for obj in objs:
+            new_objs.append(table_data[dest_table_idx](
+                **dict(
+                    map(lambda m: (m, getattr(obj, m)),
+                        map(lambda l: l.name, obj._meta.get_fields())))
+            ))
+
+        table_data[dest_table_idx].objects.using(
+            'gencache'
+        ).bulk_create(new_objs, batch_size=insert_batch_size)
+        new_objs.clear()
+        del new_objs
+        del objs
+
+        print("[Done!] Loading {} into RAM [{} records]".format(
+            table_data[0].__name__,
+            item_count
+        ))
+
+    gc.collect()
+    db.reset_queries()
+    return None
+
+
 @shared_task(queue="gencache")
 def cache_table_task_testing(idx, dest_table_idx, load_batch_size=10000, insert_batch_size=5000):
-    print(f"Inside cache_table_task_testing with index {idx}")
+    # print(f"Inside cache_table_task_testing with index {idx}")
     time.sleep(20)
     return None
 
@@ -134,13 +282,13 @@ def update_gencache(skip_run_check):
 
     # print(dest_table_index)
     # waiting for tasks within a task may lead to deadlocks, use chord and callback
-    chord(cache_table_task_testing.s(i, dest_table_index)
+    chord(cache_table_task.s(i, dest_table_index)
           for i in range(len(tables)))(completion_callback.s(running_key,
                                                              start_time))
 
 
 # https://stackoverflow.com/questions/34830964/how-to-limit-the-maximum-number-of-running-celery-tasks-by-name
-@shared_task
+@shared_task(queue="gencache")
 def update_gencache_celery(skip_run_check=False):
     try:
         update_gencache(skip_run_check)
