a89b689012591b080824bea5f0907c7b121858ae
Greenfrogs <5961364+greenfrogs@users.noreply.github.com>
2021-11-23 17:50:40 +0000

    backend dashboard: disable mailchimp in staging

--- backend/uclapi/dashboard/tasks.py
+++ backend/uclapi/dashboard/tasks.py
@@ -17,19 +17,16 @@ def add_user_to_mailing_list_task(email, name):
 
 
 def add_user_to_mailing_list(email, name):
-    data = {
-        "email_address": email,
-        "status": "subscribed",
-        "merge_fields": {
-            "EMAIL": email,
-            "FNAME": name
+    if os.environ.get("MAILCHIMP_ENDPOINT") is not None:
+        data = {
+            "email_address": email,
+            "status": "subscribed",
+            "merge_fields": {
+                "EMAIL": email,
+                "FNAME": name
+            }
         }
-    }
 
-    headers = {
-        "Authorization": "apikey {}".format(
-            os.environ["MAILCHIMP_API_KEY"])
-    }
+        headers = {"Authorization": "apikey {}".format(os.environ["MAILCHIMP_API_KEY"])}
 
-    requests.post(
-        os.environ["MAILCHIMP_ENDPOINT"], json=data, headers=headers)
+        requests.post(os.environ["MAILCHIMP_ENDPOINT"], json=data, headers=headers)
