a5494824cf379e70d7fd2b1e79139e4dfd42fe2d
Greenfrogs <5961364+greenfrogs@users.noreply.github.com>
2021-11-23 19:26:01 +0000

    backend uclapi: update celery config

--- backend/uclapi/uclapi/celery.py
+++ backend/uclapi/uclapi/celery.py
@@ -15,27 +15,23 @@ class Celery(celery.Celery):
         if os.environ.get("SENTRY_DSN") is not None:
             import sentry_sdk
             from sentry_sdk.integrations.django import DjangoIntegration
+            from sentry_sdk.integrations.celery import CeleryIntegration
 
             sentry_sdk.init(
                 dsn=os.environ.get("SENTRY_DSN"),
-                integrations=[DjangoIntegration()],
+                integrations=[DjangoIntegration(), CeleryIntegration()],
                 traces_sample_rate=0.01,
                 send_default_pii=True
             )
-
 
 app = Celery('uclapi')
 app.config_from_object('django.conf.settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@app.on_after_finalize.connect
+@app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     from celery.schedules import crontab
     sender.add_periodic_task(crontab(minute='5,35'),
                              'timetable.tasks.update_gencache_celery',
                              name='Update gencache')
-
-@app.task(bind=True)
-def task(self):
-    print('Request: {0!r}'.format(self.request))
