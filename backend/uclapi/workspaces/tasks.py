77f6eaedea30cb3ab68a73e4a235497083315806
greenfrogs <5961364+greenfrogs@users.noreply.github.com>
2021-12-03 18:10:54 +0000

    backend workspaces: add healthcheck to occupeye tasks

--- backend/uclapi/workspaces/tasks.py
+++ backend/uclapi/workspaces/tasks.py
@@ -1,4 +1,6 @@
 from celery import shared_task
+import requests
+import os
 
 from workspaces.occupeye.cache import OccupeyeCache
 from workspaces.occupeye.endpoint import TestEndpoint
@@ -14,9 +16,29 @@ def feed_occupeye_cache(test=False, mini=False):
 
 @shared_task
 def day_cache():
+    try:
+        requests.get(os.environ.get("HEALTHCHECK_OCCUPEYE_DAY") + "/start", timeout=5)
+    except requests.exceptions.RequestException:
+        pass
+
     feed_occupeye_cache.s(mini=True)()
+
+    try:
+        requests.get(os.environ.get("HEALTHCHECK_OCCUPEYE_DAY"), timeout=5)
+    except requests.exceptions.RequestException:
+        pass
 
 
 @shared_task
 def night_cache():
+    try:
+        requests.get(os.environ.get("HEALTHCHECK_OCCUPEYE_NIGHT") + "/start", timeout=5)
+    except requests.exceptions.RequestException:
+        pass
+
     feed_occupeye_cache.s(mini=False)()
+
+    try:
+        requests.get(os.environ.get("HEALTHCHECK_OCCUPEYE_NIGHT"), timeout=5)
+    except requests.exceptions.RequestException:
+        pass
