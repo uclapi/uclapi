1deafa498ef6a91627ff652ca6eb04da773b4c4b
Greenfrogs <5961364+greenfrogs@users.noreply.github.com>
2022-06-05 11:00:32 +0100

    frontend deps: migrate Swagger UI to ESM

--- frontend/src/pages/Documentation/Documentation.jsx
+++ frontend/src/pages/Documentation/Documentation.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { NavBar } from "../../components/layout/Items.jsx"
-import SwaggerUI from "swagger-ui-react"
 import "Styles/common/uclapi.scss"
 import "Styles/navbar.scss"
 import "Styles/documentation.scss"
+import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
 import spec from "../../../../uclapi.openapi.json"
 
-const Documentation = () => (
-  <div className='vertical-padding'>
-    <NavBar isScroll={false} />
-    <SwaggerUI spec={spec} />
-  </div>
-)
+function Documentation() {
+  return (
+    <div className='vertical-padding'>
+      <NavBar isScroll={false} />
+      <SwaggerUI spec={spec} />
+    </div>
+  )
+}
 
 export default Documentation
