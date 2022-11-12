95a2ef29cc0e00de427844f296e82be7ed4c2708
Shubham Jain <shubham@sjain.dev>
2022-11-12 21:26:52 +0000

    Don't accept OAuth tokens belonging to deleted apps (#3955)
    * Don't accept OAuth tokens belonging to deleted apps
    * Return 403 when token used for deleted app

--- backend/uclapi/common/decorators.py
+++ backend/uclapi/common/decorators.py
@@ -160,6 +160,16 @@ def _check_oauth_token_issues(token_code, client_secret, required_scopes):
         response.status_code = 400
         return response
 
+    if token.app.deleted:
+        response = JsonResponse({
+            "ok": False,
+            "error":
+                "The token is invalid as the developer has "
+                "deleted their app."
+        })
+        response.status_code = 403
+        return response
+
     scopes = Scopes()
     for s in required_scopes:
         if not scopes.check_scope(token.scope.scope_number, s):
